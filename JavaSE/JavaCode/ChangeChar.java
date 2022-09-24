4da33f1d53f50d09c3a5ae9c76bbfd5e8ad12376
wangbing13455 <1345529388@qq.com>
2022-09-24 11:05:59 +0800

    注释

--- JavaSE/JavaCode/ChangeChar.java
+++ JavaSE/JavaCode/ChangeChar.java
@@ -1,3 +1,8 @@
+//常见错误 ： 找不到文件（文件名写错 路径不对等 报错很清楚了） 文件名与主类名方法不一致 缺少分号
+
+
+
+
 //转义字符
 public class ChangeChar{
 	public static void main(String[] args){
@@ -12,8 +17,10 @@ public class ChangeChar{
 		//单双引号 \' \" 
 		System.out.println("守望\"守望2\"");
 		// /r 回车  注意  这里面的数字好像占据半个字符位置 有点奇怪
+		// 如果想另起一行不替换  那就\r\n
 		System.out.println("永劫无间\r守望");
 
+		System.out.println("游戏名\t价格\t销量\t游戏时长\r\n守望2\t免费\t100\t400h源氏");
 
 	}
 }

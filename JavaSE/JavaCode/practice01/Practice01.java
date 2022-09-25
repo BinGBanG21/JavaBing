dfdbe0ed5150532efafb3436b7a94a6623959fe6
wangbing <1345529388@qq.com>
2022-09-25 14:02:56 +0800

    modify code

---
+++ JavaSE/JavaCode/practice01/Practice01.java
@@ -0,0 +1,27 @@
+// hello world
+
+// jdk jre jvm的关系
+// JDK（java development kit）
+
+// JDK是Java开发工具包,是整个Java的核心，包括了Java运行环境JRE、Java工具和Java基础类库。
+
+// JRE（java runtime environment）
+
+// JRE是Java的运行环境，包括JVM标准实现及Java核心类库。
+
+// JVM（java virtual machine）
+
+// JVM是java虚拟机，是整个java实现跨平台的最核心的部分，能够运行以Java语言写作的软件程序。
+
+// 三者的关系
+
+// JDK=JRE+其他
+
+// JRE=JVM+其他
+public class Practice01{
+	public static void main(String[] args){
+		System.out.println("hello world");
+		System.out.println("姓名\t姓名\t姓名\t姓名\r\nwangbing\twangbing\twangbing\twangbing");
+		System.out.println("");
+	}
+}

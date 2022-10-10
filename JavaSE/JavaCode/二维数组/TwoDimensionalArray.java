16b4ed072800777073b4be71afab83a0e94bf3a5
wangbingbing <1345529388@qq.com>
2022-10-10 11:09:07 +0800

    数组双指针

--- JavaSE/JavaCode/二维数组/TwoDimensionalArray.java
+++ JavaSE/JavaCode/二维数组/TwoDimensionalArray.java
@@ -8,6 +8,22 @@
 	二维数组的语法
 		1) 语法: 类型[][] 数组名=new 类型[大小][大小] 
 		2) 比如: int a[][]=new int[2][3]
+			1) 一维数组的声明方式有: int[] x 或者 int x[] 
+			2) 二维数组的声明方式有: int[][] y 或者 int[] y[] 或者 int y[][] 
+			3) 二维数组实际上是由多个一维数组组成的，它的各个一维数组的长度可以相同，也可以不相同。
+			比如： map[][] 是 一个二维数组 int map [][] = {{1,2},{3,4,5}} 
+			由 map[0] 是一个含有两个元素的一维数组 
+			map[1] 是一个含有三个元素的一维数组构成，我们也称为列数不等 的二维数组
+	注意语法：int[] x,y[]   表示x是int类型的一维数组 y是int类型的二维数组
+		以下选项允许通过编译的是（b e）
+			a）x【0】=y；//错误int【】->int 
+			b）y【0】=x；//正确int【】->int【】
+			c）y【0】【0】=x；//错误int【】->int 
+			d）x【0】【0】=y；//错误x【0】【0】是错误
+			e）y【0】【0】=x【0】；//正确int->int f）x=y；//错误int【】->int【】
+			c）y【0】【0】=x；//错误int【】->int
+			d）x【0】【0】=y；//错误x【0】【0】是错误
+			f）x=y；//错误int【】->int【】
 
 */
 
@@ -54,16 +70,52 @@ public class TwoDimensionalArray {
 		*/
 		int[][] yhArr = new int[10][]
 		for (int i = 0 ; i < yhArr.length ; i++ ) {
-			int[] yhArr[i] = new int[i]
+			int[] yhArr[i] = new int[i+1]
 			for (int j = 0; i < yhArr[i].length ; j++ ) {
 				if ( i = 0 || i = yhArr[i].length - 1) {
-					yhArr[i] = 1
+					yhArr[i][j] = 1
 				} else {
-					yhArr[i] = yhArr[i-1][j] + yhArr[i-1][j-1] 
+					yhArr[i][j] = yhArr[i-1][j] + yhArr[i-1][j-1] 
 				}
 			}
 		}
 
-
+		//已知有个升序的数组，要求插入一个元素，该数组顺序依然是升序
+		//比如∶【10，12，45，90】，添加23后，数组为【10，12，23，45，90】
+		int[] arr1 =  [ 10, 12, 45, 90 ]
+		int[] arr2 = new int[arr1.length + 1]
+		int num = 23
+		int index = -1
+		//找到index
+		for (int i =0;i < arr1.length ;i++ ) {
+			if (arr1[i] >= num) {
+				index = i
+				break
+			}
+		}
+		if (index == -1) {
+			index = arr1.length
+		}
+		//找到index后 给数组扩容 然后放入 分3个阶段
+		//这是自己的想法 但是感觉还是双指针代码简介 是一个新思路 学到了
+		//原理就是用 条件判断里j++ 当条件不满足 双指针就异步了 则满足本题需要
+		// for (int i=0;i<arr2.length ; i++ ) {
+		// 	if (i<index) {
+		// 		arr2[i] = arr1[i]
+		// 	}else if (index == i) {
+		// 		arr2[i] = num
+		// 	}else{
+		// 		arr2[i+1] = arr1[i]
+		// 	}
+		// }
+		// i指的是新数组  j指向原数组
+		for (int i =0 ,j= 0; i < arr2.length; i++ ) {
+			if (index != i) {
+				arr2[i] = arr1[i]
+				j++
+			} else{
+				arr2[i] = num
+			}
+		}
 	}
 }

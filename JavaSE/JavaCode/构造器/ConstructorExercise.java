310788eb5f6ca77748d04a1395620b1af388d6ee
wangbingbing <1345529388@qq.com>
2022-10-20 08:45:19 +0800

    this

---
+++ JavaSE/JavaCode/构造器/ConstructorExercise.java
@@ -0,0 +1,16 @@
+public class ConstructorExercise {
+	public static void main(String[] args){
+
+	}
+}
+class Person {
+	String name 
+	int age
+	public Person(String pName , int pAge){
+		name = pName
+		age = pAge
+	}
+	public Person() {
+		 age = 18
+	}
+}

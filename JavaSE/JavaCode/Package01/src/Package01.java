2e71054b70dd117c20eaceba813d9ab9c7f4aa0e
wangbingbing <1345529388@qq.com>
2022-10-22 08:36:52 +0800

    package

---
+++ JavaSE/JavaCode/Package01/src/Package01.java
@@ -0,0 +1,25 @@
+/*
+*命名规则
+    只能包含数字、字母、下划线、小圆点、但不能用数字开头，不能是关键字或保留字
+    * demo.class.exec1   //错误class是关键字
+    * demo.12a           //错误12a是数字开头
+    * demo.ab12.oa       //对
+命名规范
+    一般是小写字母+小圆点一般是com.公司名.项目名.业务模块名
+    比如∶ com.wanngbing.oa.model； com.wangbing.oa.controller；举例
+    com.sina.crm.user//用户模块com.sina.crm.order//订单模块com.sina.crm.utils//工具类
+*
+*
+*
+*
+*
+*
+*
+*
+* */
+
+public class Package01 {
+    public static void main(String[] args) {
+
+    }
+}

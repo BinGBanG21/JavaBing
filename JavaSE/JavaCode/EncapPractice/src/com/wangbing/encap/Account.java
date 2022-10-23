91c63daaa28d65b3b6261f29aa23d27ed6eaf7c4
wangbingbing <1345529388@qq.com>
2022-10-23 10:10:22 +0800

    EncapPractice

---
+++ JavaSE/JavaCode/EncapPractice/src/com/wangbing/encap/Account.java
@@ -0,0 +1,59 @@
+package com.wangbing.encap;
+
+public class Account {
+    private String name;
+    private double balance;
+    private String pwd;
+
+    public Account() {
+    }
+
+    public Account(String name, double balance, String pwd) {
+        this.setName(name);
+        this.setBalance(balance);
+        this.setPwd(pwd);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 2 && name.length() <= 4) {
+            this.name = name;
+        } else {
+            System.out.println("用户ID长度违法");
+            this.name = "";
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 20) {
+            this.balance = balance;
+        } else {
+            System.out.println("余额不能小于20");
+        }
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        if (pwd.length() == 6) {
+            this.pwd = pwd;
+        } else {
+            System.out.println("用户密码错误");
+            this.pwd = "";
+        }
+    }
+
+    public void showInfo() {
+        System.out.println("账号信息 name=" + name + " 余额=" + balance + " 密码" + pwd);
+    }
+
+}

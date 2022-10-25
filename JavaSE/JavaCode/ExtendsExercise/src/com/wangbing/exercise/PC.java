83c390fad1b6c5aa964b6480f6d3e37ad122c08c
wangbingbing <1345529388@qq.com>
2022-10-25 17:51:02 +0800

    ExtendsPractice

---
+++ JavaSE/JavaCode/ExtendsExercise/src/com/wangbing/exercise/PC.java
@@ -0,0 +1,21 @@
+package com.wangbing.exercise;
+
+public class PC extends Computer {
+    private String brand;
+
+    public PC(String cpu, int memory, int disk, String brand) {
+        super(cpu, memory, disk);
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void showInfo(){
+        System.out.println(getDetails()+"brand"+this.brand);
+    }
+}

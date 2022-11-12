6fd721bf40856d4049f35939a552c67855cb1973
wangbingbing <1345529388@qq.com>
2022-11-12 15:01:12 +0800

    Exception

--- JavaSE/Expection/src/Exception.java
+++ JavaSE/Expection/src/Exception.java
@@ -53,11 +53,69 @@
             //1.不管try代码块是否有异常发生，始终要执行finally
             //2.所以，通常将释放资源的代码，放在finally
         }
+    try-catch 方式处理异常-注意事项
+        1）如果异常发生了，则异常发生后面的代码不会执行，直接进入到catch块，
+        2）如果异常没有发生，则顺序执行try的代码块，不会进入到catch.
+        3）如果希望不管是否发生异常，都执行某段代码（比如关闭连接，释放资源等）则使用如下代码-finally{}
+        4）可以有多个catch语句，捕获不同的异常（进行不同的业务处理），要求父类异常在后，子类异常在前
+          （其实就是在catch（中写出具体的Exception的子类型））
+           比如（Exception在后，NullPointerException在前），如果发生异常，只会匹配一个catch
+        5）可以进行 try-finally 配合使用，这种用法相当于没有捕获异常，因此程序会直接崩掉/退出。
+           应用场景，就是执行一段代码，不管是否发生异常，都必须执行某个业务逻辑
+    try-catch-finally 执行顺序总结
+        1）如果没有出现异常，则执行try块中所有语句，不执行catch块中语句
+           如果有finally，最后还需要执行finally里面的语句
+        2）如果出现异常，则try块中异常发生后，try块剩下的语句不再执行。将执行catch块中的语句
+           如果有finally，最后还需要执行finally里面的语句!
+    throws 异常处理
+        1）如果一个方法（中的语句执行时）可能生成某种异常，但是并不能确定如何处理这种异常
+           则此方法应显示地声明抛出异常，表明该方法将不对这些异常进行处理，而由该方法的调用者负责处理。
+        2）在方法声明中用throws语句可以声明抛出异常的列表，throws后面的异常类型可以是方法中产生的异常类型，也可以是它的父类。
+    注意事项和使用细节
+        1）对于编译异常，程序中必须处理，比如try-catch或者throws
+        2）对于运行时异常，程序中如果没有处理，默认就是throws的方式处理
+        3）子类重写父类的方法时，对抛出异常的规定∶子类重写的方法，所抛出的异常类型要
+           么和父类抛出的异常一致，要么为父类抛出的异常的类型的子类型
+        4）在throws过程中，如果有方法try-catch，就相当于处理异常，就可以不必throws
+    自定义异常
+        当程序中出现了某些“错误”，但该错误信息并没有在Throwable子类中描述处理，这个时候可以自己设计异常类，用于描述该错误信息。
+    自定义异常的步骤
+        1）定义类∶自定义异常类名（程序员自己写）继承Exception或RuntimeException
+        2）如果继承Exception，属于编译异常
+        3）如果继承RuntimeException，属于运行异常（一般来说，继承RuntimeException）
+
 
 * */
 
-public class Exception {
+public class Exception extends Throwable {
     public static void main(String[] args) {
+        //1.如果 try 代码块有可能有多个异常
+        //2.可以使用多个 catch 分别捕获不同的异常，相应处理
+        //3.要求子类异常写在前面，父类异常写在后面
+        try {
+            Person person = new Person();
+            //person = null;
+            System.out.println(person.getName());//NullPointerException
+            int n1 = 10;
+            int n2 = 0;
+            int res = n1 / n2; //ArithmeticException
+        } catch (NullPointerException e) {
+            System.out.println("空指针异常=" + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("算术异常=" + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("最终执行...");
+        }
+    }
+}
 
+
+class Person {
+    private String name = "jack";
+
+    public String getName() {
+        return name;
     }
 }

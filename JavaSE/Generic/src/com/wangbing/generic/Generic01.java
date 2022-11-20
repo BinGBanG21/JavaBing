4464f6d8c2d2ea92363b05eda5b11ae654292468
wangbingbing <1345529388@qq.com>
2022-11-20 09:21:35 +0800

    Generic

--- JavaSE/Generic/src/com/wangbing/generic/Generic01.java
+++ JavaSE/Generic/src/com/wangbing/generic/Generic01.java
@@ -15,6 +15,24 @@ package com.wangbing.generic;/*
             2）在类声明或实例化时只要指定好需要的具体的类型即可。
             3）Java泛型可以保证如果程序在编译时没有发出警告，运行时就不会产生ClassCastException异常。同时，代码更加简洁、健壮
             4）泛型的作用是可以在类声明时通过一个标识表示类中某个属性的类型，或者是某个方法的返回值的类型，或者是参数类型。
+        泛型的语法
+            interface接口 <T> 和 class类<K，V> //比如∶List，ArrayList
+                1）其中，T，K，V不代表值，而是表示类型。
+                2）任意字母都可以。常用T表示，是Type的缩写
+        泛型的实例化
+            要在类名后面指定类型参数的值（类型）。如∶
+                1）List<String>strList=newArrayList<String>0；
+                2) Iterator<Customer> iterator= customers.iterator();
+        泛型使用的注意事项和细节
+            1. interface List<T>{}，public class HashSet<E>{}..等等
+            说明：T，E只能是引用类型看看下面语句是否正确
+                List<Integer>list=newArrayList<Integer>();//OK
+                List<int>list2=newArrayList<int>0∶//错误
+            2.在给泛型指定具体类型后，可以传入该类型或者其子类类型
+            3. 泛型使用形式
+                List<Integer>list1=newArrayList<Integer>();
+                List<Integer>list2=newArrayList<>()；
+            3.如果我们这样写 List list3= new ArrayList()；默认给它的 泛型是【<E> E就是 Object】
  **/
 
 import java.util.ArrayList;

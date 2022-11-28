9b05d6b8beb617f727fd2dfb26b080c5d1362a85
wangbingbing <1345529388@qq.com>
2022-11-28 09:37:06 +0800

    Socket

---
+++ JavaSE/Socket_/src/com/wangbing/socket/SocketTCP01Client.java
@@ -0,0 +1,29 @@
+package com.wangbing.socket;/*
+ * @author WangBing
+ * @version 1.0
+ **/
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketTCP01Client {
+    public static void main(String[] args) throws IOException {
+        //思路
+        //1. 连接服务端 (ip , 端口）
+        //解读: 连接本机的 9999 端口, 如果连接成功，返回 Socket 对象
+        Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
+        System.out.println("客户端 socket 返回=" + socket.getClass());
+        //2. 连接上后，生成 Socket, 通过 socket.getOutputStream()
+        // 得到 和 socket 对象关联的输出流对象
+        OutputStream outputStream = socket.getOutputStream();
+        //3. 通过输出流，写入数据到 数据通道
+        outputStream.write("hello, server".getBytes());
+        //4. 关闭流对象和 socket, 必须关闭
+        outputStream.close();
+        socket.close();
+        System.out.println("客户端退出.....");
+    }
+}

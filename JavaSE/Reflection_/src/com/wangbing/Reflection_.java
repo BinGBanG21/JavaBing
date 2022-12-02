091ec39cf36f6dd385bf72a195bf74082b8fa7ef
wangbingbing <1345529388@qq.com>
2022-12-02 10:42:07 +0800

    Reflection

--- JavaSE/Reflection_/src/com/wangbing/Reflection_.java
+++ JavaSE/Reflection_/src/com/wangbing/Reflection_.java
@@ -12,6 +12,18 @@ import java.util.Properties;
               并能操作对象的属性及方法。反射在设计模式和框架底层都会用到
             2.加载完类之后，在堆中就产生了一个Class类型的对象（一个类只有一个Class对象），这个对象包含了类的完整结构信息。
               通过这个对象得到类的结构。这个Class对象就像一面镜子，透过这个镜子看到类的结构，所以，形象的称之为反射
+        Java 反射机制可以完成
+            1.在运行时判断任意一个对象所属的类
+            2.在运行时构造任意一个类的对象
+            3.在运行时得到任意一个类所具有的成员变量和方法
+            4.在运行时调用任意一个对象的成员变量和方法
+            5.生成动态代理
+        反射相关的主要类
+            1.java.lang.Class∶代表一个类，Class对象表示某个类加载后在堆中的对象
+            2.java.lang.reflect.Method∶代表类的方法，Method对象表示某个类的方法
+            3.java.lang.reflect.Field∶代表类的成员变量，Field对象表示某个类的成员变量
+            4.java.lang.reflect.Constructor∶代表类的构造方法，Constructor对象表示
+构造器
 * */
 @SuppressWarnings({"all"})
 public class Reflection_ {

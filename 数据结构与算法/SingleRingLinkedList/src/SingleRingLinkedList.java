6a7f10d03768697169b2ab4b28c4ba4871503ed5
wangbingbing <1345529388@qq.com>
2022-12-13 19:12:23 +0800

    SingleRingLinkedList

--- 数据结构与算法/SingleRingLinkedList/src/SingleRingLinkedList.java
+++ 数据结构与算法/SingleRingLinkedList/src/SingleRingLinkedList.java
@@ -25,9 +25,27 @@ public class SingleRingLinkedList {
 class CircleSingleLinkedList {
     //初始化first节点
     private node firstNode = null;
+    private int size;
+
+    public node getFirstNode() {
+        return firstNode;
+    }
+
+    public void setFirstNode(node firstNode) {
+        this.firstNode = firstNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     //添加节点 构建一个环形链表
-    public void addNode(int nums){
-        if (nums < 1){
+    public void addNode(int nums) {
+        if (nums < 1) {
             System.out.println("至少添加一个节点");
         }
         //for循环生成环形节点
@@ -35,31 +53,70 @@ class CircleSingleLinkedList {
         //然后cur.next指向newNode  newNode.next指向first 就完成了环形链表
         node cur = null;
 
-        for (int i = 0; i <nums ; i++) {
-            node newNode  = new node(i);
+        for (int i = 0; i < nums; i++) {
+            node newNode = new node(i);
             if (i == 1) {
                 firstNode = newNode;
                 firstNode.setNext(firstNode); //构成了环状
                 cur = firstNode; //因为first不能动 让cur保存first
-            }else{
+            } else {
                 cur.setNext(newNode);
                 newNode.setNext(firstNode);
                 cur = newNode;
             }
+            size++;
         }
     }
 
     //遍历当前的链表
-    public void list (){
-        if (firstNode.getNext() == null){
+    public void list() {
+        if (firstNode.getNext() == null) {
             System.out.println("链表为空 先添加元素吧");
         }
         node cur = firstNode.getNext();
         //遍历完毕的条件为firstNode
-        while (cur!=firstNode){
+        while (cur != firstNode) {
             System.out.println(cur);
             cur = cur.getNext();
         }
+    }
+
+    //根据数字删除节点（其实就是目标节点和前一个节点）
+    //核心逻辑说就是：
+    //first指向头节点 然后创建一个辅助指针(变量) helper ,事先应该指向环形链表的最后这个节点.
+    //当数数时，让first和helper指针同时的移动 count -1 次
+    //这时就可以将first指向的节点出圈
+    //first= first.next
+    //helper.next=first
+    //原来first指向的节点就没有任何引用，就会被回收
+    public void removeByCount(int count) {
+        if (count < 2 || count > getSize()) {
+            System.out.println("输入的数字不合法 应在2-" + size + "之间");
+        }
+        node helper = firstNode;
+        //遍历当前链表 找到最后一个节点 赋值为helper
+        while (firstNode.getNext() != firstNode) {
+            helper = helper.getNext();
+        }
+        //经过while循环 helper就指向链表的尾节点
+        //移动距离为count - 1
+        int num = 0;
+        node cur = firstNode.getNext();
+        while (true){
+            if (firstNode == helper){
+                break;
+            }
+            //双指针移动 找到要移除的节点
+            while (num != count - 1) {
+                helper = helper.getNext();
+                firstNode = firstNode.getNext();
+            }
+            //firstNode指向的就是要出圈的节点
+            System.out.printf("第%d个节点出圈\n",firstNode.getNo());
+            firstNode = firstNode.getNext();
+            helper.setNext(firstNode);
+        }
+        System.out.printf("最后留在圈中的节点为%d",firstNode.getNo());
     }
 }
 

414e25654f6b7bed54d1d0bbbf7abf323033a0d4
wangbingbing <1345529388@qq.com>
2022-12-23 10:42:34 +0800

    hash

--- 数据结构与算法/Search/src/BinarySearch.java
+++ 数据结构与算法/Search/src/BinarySearch.java
@@ -13,6 +13,8 @@
 
  * */
 
+import java.util.ArrayList;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 8, 10, 89, 1000, 1000, 1234};
@@ -20,9 +22,9 @@ public class BinarySearch {
 
     }
 
-    public static int binary(int[] arr, int left, int right, int findVal) {
+    public static ArrayList<Integer> binary(int[] arr, int left, int right, int findVal) {
         if (left > right) { //说明没有找到 left = mid + 1; right = mid -1 ;
-            return -1;
+            return new ArrayList<Integer>();
         }
         //二分查找其实就是用中间值比较 然后移动left和right
         int mid = (left + right) / 2;
@@ -34,7 +36,34 @@ public class BinarySearch {
         } else if (arr[mid] < findVal) { //向右递归
             return binary(arr, mid + 1, right, findVal);
         } else {
-            return mid;
+            //思路分析
+            //1.在找到mid索引值,不要马上返回
+            //2.向mid索引值的左边扫描，将所有满足1000，的元素的下标，加入到集合ArrayList
+            //3.向mid索引值的右边扫描，将所有满足1000，的元素的下标，加入到集合ArrayList
+            //4.将Arraylist返回
+            //return mid;
+            ArrayList<Integer> list = new ArrayList<>();
+            //先往左边找
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                list.add(temp);
+                temp--;
+            }
+            //在把中间值mid放入list中记录
+            list.add(mid);
+            temp = mid + 1;
+            //然后向右扫描
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != findVal) {
+                    break;
+                }
+                list.add(temp);
+                temp++;
+            }
+            return list;
         }
     }
 }

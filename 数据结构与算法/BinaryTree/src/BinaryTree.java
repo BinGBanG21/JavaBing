b6394c09efd88fc7c56033416b2bce1153dc15ea
wangbingbing <1345529388@qq.com>
2022-12-25 12:56:58 +0800

    BinaryTree

--- 数据结构与算法/BinaryTree/src/BinaryTree.java
+++ 数据结构与算法/BinaryTree/src/BinaryTree.java
@@ -44,18 +44,19 @@ public class BinaryTree {
         HeroNode node1 = new HeroNode(2, "玉麒麟");
         HeroNode node2 = new HeroNode(3, "浪子");
         HeroNode node3 = new HeroNode(4, "豹子头");
+        HeroNode node4 = new HeroNode(5, "大刀");
 
         //手动方式创建二叉树
         binaryTree1.setRoot(root);
         root.setLeft(node1);
         root.setRight(node2);
         node2.setRight(node3);
-
+        node3.setLeft(node4);
         binaryTree1.preOrder();
     }
 }
 
-class BinaryTree1{
+class BinaryTree1 {
     private HeroNode root;
 
     public void setRoot(HeroNode root) {
@@ -63,27 +64,72 @@ class BinaryTree1{
     }
 
     //前序遍历
-    public void preOrder(){
-        if (this.root!=null){
+    public void preOrder() {
+        if (this.root != null) {
             this.root.preOrder();
-        }else{
+        } else {
             System.out.println("树为空 无法遍历");
         }
     }
+
+
     //中序遍历
-    public void infixOrder(){
-        if (this.root!=null){
+    public void infixOrder() {
+        if (this.root != null) {
             this.root.infixOrder();
-        }else{
+        } else {
             System.out.println("树为空 无法遍历");
         }
     }
+
     //后序遍历
-    public void postOrder(){
-        if (this.root!=null){
+    public void postOrder() {
+        if (this.root != null) {
             this.root.postOrder();
-        }else{
+        } else {
             System.out.println("树为空 无法遍历");
+        }
+    }
+
+    //前序查找
+    //都是对root进行一个空值判断
+    public HeroNode preOrderSearch(int no) {
+        if (root != null) {
+            return root.postSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    //中序查找
+    public HeroNode infixOrderSearch(int no) {
+        if (root != null) {
+            return root.postSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    //后序查找
+    public HeroNode postOrderSearch(int no) {
+        if (root != null) {
+            return root.postSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    //删除节点
+    public void delNode(int no) {
+        if (root != null) {
+            if (root.getNo() == no){
+                root = null;
+            }else {
+                delNode(no);
+            }
+
+        } else {
+            System.out.println("空树 不能删除");
         }
     }
 }
@@ -173,5 +219,88 @@ class HeroNode {
             this.right.preOrder();
         }
         System.out.println(this);
+    }
+
+    //前序查找
+    public HeroNode preSearch(int no) {
+        //如果当前节点是 return当前节点
+        if (this.no == no) {
+            return this;
+        }
+        //如果当前节点不是 且左节点不为空 则递归左侧查找
+        //且必须要事先声明result节点 不然没法做错误的判断
+        HeroNode result = null;
+        if (this.getLeft() != null) {
+            result = this.left.preSearch(no);
+            if (result != null) {
+                return result;
+            }
+        }
+        //如果在递归左子节点的时候找到了 就赋值判断返回 否则递归右节点 直接返回即可
+        if (this.getRight() != null) {
+            result = this.right.preSearch(no);
+        }
+        return result;
+    }
+
+    //中序遍历查找
+    public HeroNode infixSearch(int no) {
+        HeroNode result = null;
+        if (this.getLeft() != null) {
+            result = this.left.infixSearch(no);
+        }
+        if (result != null) {
+            return result;
+        }
+        if (this.no == no) {
+            return this;
+        }
+        if (this.getRight() != null) {
+            result = this.right.infixSearch(no);
+        }
+        //找到了 就会返回 找不到  那就返回前面的null
+        return result;
+    }
+
+    //后序遍历查找
+    public HeroNode postSearch(int no) {
+        HeroNode result = null;
+        if (this.getLeft() != null) {
+            result = this.left.postSearch(no);
+        }
+        if (result != null) {
+            return result;
+        }
+        if (this.getRight() != null) {
+            result = this.right.postSearch(no);
+        }
+        if (result != null) {
+            return result;
+        }
+        if (this.no == no) {
+            return this;
+        }
+        return result;
+    }
+
+    //二叉树删除节点
+    //思路：因为我们的二叉树是单向的 所以我们在删除节点的时候 不能找到当前节点
+    //要找到当前节点的下一个节点 然后才能删除
+    public void delNode(int no) {
+        if (this.left != null && this.left.no == no) {
+            this.left = null;
+            return;
+        }
+        if (this.right != null && this.right.no == no) {
+            this.right = null;
+            return;
+        }
+        //如果都没找到 那么递归向下找
+        if (this.left != null) {
+            this.left.delNode(no);
+        }
+        if (this.right != null) {
+            this.right.delNode(no);
+        }
     }
 }

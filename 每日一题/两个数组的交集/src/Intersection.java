040c6903cdb2de4bbfe2a66d28c9fbda0e0c2802
wangbingbing <1345529388@qq.com>
2023-02-14 10:15:00 +0800

    Intersection

---
+++ 每日一题/两个数组的交集/src/Intersection.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Intersection {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        //边界处理
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+        //把第一个数组中的元素放入
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                resSet.add(i);
+            }
+        }
+        //把resSet变成数组 返回
+        return resSet.stream().mapToInt(x -> x).toArray();
+    }
+}

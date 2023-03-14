cf035674d0fe9d7804deedea3d074a4513a5c52d
wangbingbing <1345529388@qq.com>
2023-03-14 22:27:21 +0800

    BinaryTreePaths

---
+++ 每日一题/平衡二叉树/src/IsBalanced.java
@@ -0,0 +1,41 @@
+
+public class IsBalanced {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

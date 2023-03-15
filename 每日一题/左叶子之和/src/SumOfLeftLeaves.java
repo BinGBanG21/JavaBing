c02cc0b6709259f65a5fca757fa7df7e43655e58
wangbingbing <1345529388@qq.com>
2023-03-15 20:28:53 +0800

    SumOfLeftLeaves

---
+++ 每日一题/左叶子之和/src/SumOfLeftLeaves.java
@@ -0,0 +1,42 @@
+public class SumOfLeftLeaves {
+    public static void main(String[] args) {
+
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        return root != null ? dfs(root) : 0;
+    }
+
+    public int dfs(TreeNode node) {
+        int ans = 0;
+        if (node.left != null) {
+            ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+        }
+        if (node.right != null && !isLeafNode(node.right)) {
+            ans += dfs(node.right);
+        }
+        return ans;
+    }
+
+    public boolean isLeafNode(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

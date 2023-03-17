a1946ae6e63978d93411f7c202ca1878173afa96
wangbingbing <1345529388@qq.com>
2023-03-17 22:01:37 +0800

    FindBottomLeftValue

---
+++ 每日一题/找树左下角的值/src/FindBottomLeftValue.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class FindBottomLeftValue {
+    public static void main(String[] args) {
+
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        int ret = 0;
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode p = queue.poll();
+            if (p.right != null) {
+                queue.offer(p.right);
+            }
+            if (p.left != null) {
+                queue.offer(p.left);
+            }
+            ret = p.val;
+        }
+        return ret;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

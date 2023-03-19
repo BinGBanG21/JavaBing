3e3a56267d7f73a34c7185d95836c3f339fbf38f
wangbingbing <1345529388@qq.com>
2023-03-19 20:45:07 +0800

    ConstructMaximumBinaryTree

---
+++ 每日一题/最大二叉树/src/ConstructMaximumBinaryTree.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConstructMaximumBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        int n = nums.length;
+        List<Integer> stack = new ArrayList<Integer>();
+        TreeNode[] tree = new TreeNode[n];
+        for (int i = 0; i < n; ++i) {
+            tree[i] = new TreeNode(nums[i]);
+            while (!stack.isEmpty() && nums[i] > nums[stack.get(stack.size() - 1)]) {
+                tree[i].left = tree[stack.get(stack.size() - 1)];
+                stack.remove(stack.size() - 1);
+            }
+            if (!stack.isEmpty()) {
+                tree[stack.get(stack.size() - 1)].right = tree[i];
+            }
+            stack.add(i);
+        }
+        return tree[stack.get(0)];
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

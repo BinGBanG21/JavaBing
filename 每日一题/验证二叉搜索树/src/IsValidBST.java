dbdf9fae1943b2517c089ac5865d572c9fa66f42
wangbingbing <1345529388@qq.com>
2023-03-20 20:34:39 +0800

    IsValidBST

---
+++ 每日一题/验证二叉搜索树/src/IsValidBST.java
@@ -0,0 +1,40 @@
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class IsValidBST {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }

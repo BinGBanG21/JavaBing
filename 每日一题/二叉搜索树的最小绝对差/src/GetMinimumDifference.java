1bc143fb94fffd9afa87f4f1d92e67f65a5933d6
wangbingbing <1345529388@qq.com>
2023-03-22 19:44:38 +0800

    GetMinimumDifference

---
+++ 每日一题/二叉搜索树的最小绝对差/src/GetMinimumDifference.java
@@ -0,0 +1,49 @@
+
+public class GetMinimumDifference {
+    int pre;
+    int ans;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs(root.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

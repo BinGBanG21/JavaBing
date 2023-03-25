2e0b0502eeba490e6dc98123fd0e2a3bc4c396fc
wangbingbing <1345529388@qq.com>
2023-03-25 15:54:41 +0800

    LowestCommonAncestor

---
+++ 每日一题/二叉搜索树的最近公共祖先/src/LowestCommonAncestor.java
@@ -0,0 +1,31 @@
+
+
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //一共3种情况 > < =
+        if (root.val > p.val && root.val > q.val) {
+            //向左走
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            //向右走
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        //如果root在pq之间 说明正好是最近的公共祖先
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}

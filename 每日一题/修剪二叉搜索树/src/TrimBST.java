b860a2777c002574dcb9f1beca38af60c65a94a6
wangbingbing <1345529388@qq.com>
2023-03-28 22:53:08 +0800

    TrimBST

---
+++ 每日一题/修剪二叉搜索树/src/TrimBST.java
@@ -0,0 +1,52 @@
+public class TrimBST {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        while (root != null && (root.val < low || root.val > high)) {
+            if (root.val < low) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        if (root == null) {
+            return null;
+        }
+        for (TreeNode node = root; node.left != null; ) {
+            if (node.left.val < low) {
+                node.left = node.left.right;
+            } else {
+                node = node.left;
+            }
+        }
+        for (TreeNode node = root; node.right != null; ) {
+            if (node.right.val > high) {
+                node.right = node.right.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

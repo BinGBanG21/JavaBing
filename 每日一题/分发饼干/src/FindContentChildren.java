8c5a85f6e699e0d858beaf3b9c4ad77d1adfda3b
wangbingbing <1345529388@qq.com>
2023-04-08 23:10:01 +0800

    FindContentChildren

---
+++ 每日一题/分发饼干/src/FindContentChildren.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class FindContentChildren {
+    public static void main(String[] args) {
+
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int m = g.length, n = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < m && j < n; i++, j++) {
+            while (j < n && g[i] > s[j]) {
+                j++;
+            }
+            if (j < n) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

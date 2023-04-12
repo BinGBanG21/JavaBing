069e3e946b710af8c91c4c33f3c214549f2c817e
wangbingbing <1345529388@qq.com>
2023-04-12 20:40:11 +0800

    Jump

---
+++ 每日一题/跳跃游戏 II/src/Jump.java
@@ -0,0 +1,20 @@
+public class Jump {
+    public static void main(String[] args) {
+
+    }
+
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}

197d1b52ea4c0733eac0b56fcbd6b03ad1f538bb
wangbingbing <1345529388@qq.com>
2023-04-13 08:26:36 +0800

    Repeat

--- 每日一题/二分查找/src/Search.java
+++ 每日一题/二分查找/src/Search.java
@@ -3,19 +3,19 @@ public class Search {
 
     }
 
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
+//    public int search(int[] nums, int target) {
+//        int left = 0;
+//        int right = nums.length - 1;
+//        while (left <= right) {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] < target) {
+//                left = mid + 1;
+//            } else if (nums[mid] > target) {
+//                right = mid - 1;
+//            } else {
+//                return mid;
+//            }
+//        }
+//        return -1;
+//    }
 }

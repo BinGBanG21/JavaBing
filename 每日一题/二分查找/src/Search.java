a7fe30fbc890f25450c98a7160d69dbbb0837c14
wangbingbing <1345529388@qq.com>
2023-04-15 15:39:23 +0800

    Repeat

--- 每日一题/二分查找/src/Search.java
+++ 每日一题/二分查找/src/Search.java
@@ -3,7 +3,7 @@ public class Search {
 
     }
 
-//    public int search(int[] nums, int target) {
+    //    public int search(int[] nums, int target) {
 //        int left = 0;
 //        int right = nums.length - 1;
 //        while (left <= right) {
@@ -18,4 +18,30 @@ public class Search {
 //        }
 //        return -1;
 //    }
+    //循环不变量 左闭右闭 left 和 right可以相等 左闭右开 left 和 right 不能相等 举例 [1 ,1] [ 1, 1)
+    //左闭右闭写法
+    public int search(int[] nums, int target) {
+        //合法边界判断 避免target不合理却仍多次循环
+        if( target < nums[0] || target > nums[nums.length -1]){
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        //开始循环
+        while (left <= right) { // left < right
+            //每次重新获取中间值
+            int middle = (left + right) / 2;
+            //利用中间值判断 更新右区间的左边界
+            if (nums[middle] < target) {
+                left = middle + 1; // 不变 因为左区间是闭合
+            } else if (nums[middle] > target) { //更新左区间的右边界
+                right = middle - 1; //right = middle;
+            } else {
+                return middle;
+            }
+        }
+        //循环结束没找到 return -1
+        return -1;
+    }
+
 }
--- 每日一题/移除元素/src/RemoveElement.java
+++ 每日一题/移除元素/src/RemoveElement.java
@@ -3,16 +3,32 @@ public class RemoveElement {
 
     }
 
-    public int removeElement(int[] nums, int val) {
-        //fast找到非目标元素
-        //slow用来刷新数组
+//    public int removeElement(int[] nums, int val) {
+//        //fast找到非目标元素
+//        //slow用来刷新数组
+//        int slow = 0;
+//        for (int fast = 0; fast < nums.length; fast++) {
+//            if (nums[fast] != val) {
+//                nums[slow] = nums[fast];
+//                slow++;
+//            }
+//        }
+//        return slow;
+//    }
+    //数组是一个连续的内存空间 不能删除元素 只能覆盖 一些语言的api 也只是帮你做了一个包装 本质上内存空间还是不变的
+    //暴力解 两层for循环 找到 目标元素 然后一个个元素向后覆盖
+    //优化 双指针 双指针其实就是把双重for循环变成1层for循环
+    //两个指针都从头开始 其实双指针无非就是两个头 一头一尾 或者从头 挨着
+    //fast指针找到我们需要的元素 并且利用slow给新数组赋值
+    public int removeElement(int[] nums, int val){
         int slow = 0;
-        for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != val) {
+        for(int fast = 0; fast < nums.length;fast++){
+            //找到新数组需要的元素
+            if(nums[fast] != val){
                 nums[slow] = nums[fast];
+                //对新数组赋值
                 slow++;
             }
         }
-        return slow;
     }
 }

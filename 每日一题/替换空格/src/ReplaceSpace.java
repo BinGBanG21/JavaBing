847703f8e500cf381ab22e25d1d0e8f2d190212f
wangbingbing <1345529388@qq.com>
2023-05-03 15:45:52 +0800

    Repeat

--- 每日一题/替换空格/src/ReplaceSpace.java
+++ 每日一题/替换空格/src/ReplaceSpace.java
@@ -3,39 +3,58 @@ public class ReplaceSpace {
 
     }
 
+    //    public String replaceSpace(String s) {
+//        if (s == null || s.length() == 0) {
+//            return s;
+//        }
+//        //对原字符串进行扩容
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == ' ') {
+//                sb.append("  ");
+//            }
+//        }
+//        //然后对扩容部分做一个检查
+//        if (sb.length() == 0) {
+//            return s;
+//        }
+//        //如果有空格 双指针法
+//        //左指针 指向原字符串最后一个字符
+//        int left = s.length() - 1;
+//        s += sb.toString();
+//        //右指针 指向新字符串最后一个字符
+//        int right = s.length() - 1;
+//        char[] ch = s.toCharArray();
+//        while (left >= 0) {
+//            if (ch[left] == ' ') {
+//                ch[right--] = '0';
+//                ch[right--] = '2';
+//                ch[right] = '%';
+//            } else {
+//                ch[right] = ch[left];
+//            }
+//            left--;
+//            right--;
+//        }
+//        return new String(ch);
+//    }
     public String replaceSpace(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        //对原字符串进行扩容
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                sb.append("  ");
+            //错误处理
+            if (s == null || s.length() == 0) {
+                return s;
             }
-        }
-        //然后对扩容部分做一个检查
-        if (sb.length() == 0) {
-            return s;
-        }
-        //如果有空格 双指针法
-        //左指针 指向原字符串最后一个字符
-        int left = s.length() - 1;
-        s += sb.toString();
-        //右指针 指向新字符串最后一个字符
-        int right = s.length() - 1;
-        char[] ch = s.toCharArray();
-        while (left >= 0) {
-            if (ch[left] == ' ') {
-                ch[right--] = '0';
-                ch[right--] = '2';
-                ch[right] = '%';
-            } else {
-                ch[right] = ch[left];
+            //创建字符数组 假设所有都是空格
+            char[] ch = new char[s.length() * 3];
+            int index = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ' ') {
+                    ch[index++] ='%';
+                    ch[index++] ='2';
+                    ch[index++] ='0';
+                } else {
+                    ch[index++] =s.charAt(i);
+                }
             }
-            left--;
-            right--;
-        }
-        return new String(ch);
+            return new String(ch, 0, index);
     }
 }

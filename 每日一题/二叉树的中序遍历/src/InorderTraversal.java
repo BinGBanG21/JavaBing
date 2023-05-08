84183b909871245ad10b2c1d37eff5289be8338b
wangbingbing <1345529388@qq.com>
2023-05-08 12:51:43 +0800

    Repeat

--- 每日一题/二叉树的中序遍历/src/InorderTraversal.java
+++ 每日一题/二叉树的中序遍历/src/InorderTraversal.java
@@ -8,31 +8,49 @@ public class InorderTraversal {
 
     }
 
+    //    public List<Integer> inorderTraversal(TreeNode root) {
+//        //递归的逻辑其实可以用栈去模拟 函数递归 开执行栈 然后栈顶先执行
+//        //但是没有那么简单 所以比较简单的递归 我们可以使用栈去模拟
+//        //思路：因为中序遍历 先处理左
+//        //一直循环直到左边节点都加入到栈中
+//        //然后开始填充返回的List 先把左节点加入
+//        List<Integer> list = new ArrayList<>();
+//        //创建栈 用来管理node
+//        Stack<TreeNode> stack = new Stack<>();
+//        //循环 先添加左节点到栈中
+//        while (stack.size() > 0 || root != null) {
+//            //向栈中添加节点 很关键的一步
+//            if (root != null) {
+//                stack.add(root);
+//                root = root.left;
+//            } else { //从栈中输出结果
+//                TreeNode res = stack.pop();
+//                list.add(res.val);
+//                //很关键的一步 如果root节点没有右节点 那么就为null 那么会继续弹栈
+//                //如果当前节点有右节点 那就不做弹栈操作 先做入栈
+//                root = res.right;
+//            }
+//        }
+//        return list;
+//    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        //递归的逻辑其实可以用栈去模拟 函数递归 开执行栈 然后栈顶先执行
-        //但是没有那么简单 所以比较简单的递归 我们可以使用栈去模拟
-        //思路：因为中序遍历 先处理左
-        //一直循环直到左边节点都加入到栈中
-        //然后开始填充返回的List 先把左节点加入
-        List<Integer> list = new ArrayList<>();
-        //创建栈 用来管理node
-        Stack<TreeNode> stack = new Stack<>();
-        //循环 先添加左节点到栈中
-        while (stack.size() > 0 || root != null) {
-            //向栈中添加节点 很关键的一步
-            if (root != null) {
-                stack.add(root);
-                root = root.left;
-            } else { //从栈中输出结果
-                TreeNode res = stack.pop();
-                list.add(res.val);
-                //很关键的一步 如果root节点没有右节点 那么就为null 那么会继续弹栈
-                //如果当前节点有右节点 那就不做弹栈操作 先做入栈
-                root = res.right;
-            }
-        }
-        return list;
+        //创建结果
+        List<Integer> result = new ArrayList<>();
+        inOrder(root, result);
+        return result;
     }
+
+    public void inOrder(TreeNode cur, List<Integer> result) {
+        //终止条件
+        if (cur == null) {
+            return;
+        }
+        //左 右 中
+        inOrder(cur.left, result);
+        result.add(cur.val);
+        inOrder(cur.right, result);
+    }
+
 
     public class TreeNode {
         int val;
--- 每日一题/二叉树的前序遍历/src/PreorderTraversal.java
+++ 每日一题/二叉树的前序遍历/src/PreorderTraversal.java
@@ -7,34 +7,52 @@ public class PreorderTraversal {
 
     }
 
+    //    public List<Integer> preorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<Integer>();
+//        if (root == null) {
+//            return res;
+//        }
+//
+//        TreeNode p1 = root, p2 = null;
+//
+//        while (p1 != null) {
+//            p2 = p1.left;
+//            if (p2 != null) {
+//                while (p2.right != null && p2.right != p1) {
+//                    p2 = p2.right;
+//                }
+//                if (p2.right == null) {
+//                    res.add(p1.val);
+//                    p2.right = p1;
+//                    p1 = p1.left;
+//                    continue;
+//                } else {
+//                    p2.right = null;
+//                }
+//            } else {
+//                res.add(p1.val);
+//            }
+//            p1 = p1.right;
+//        }
+//        return res;
+//    }
+    //二叉树前序遍历 递归
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
-        if (root == null) {
-            return res;
-        }
+        //创建结果
+        List<Integer> result = new ArrayList<>();
+        preOrder(root, result);
+        return result;
+    }
 
-        TreeNode p1 = root, p2 = null;
-
-        while (p1 != null) {
-            p2 = p1.left;
-            if (p2 != null) {
-                while (p2.right != null && p2.right != p1) {
-                    p2 = p2.right;
-                }
-                if (p2.right == null) {
-                    res.add(p1.val);
-                    p2.right = p1;
-                    p1 = p1.left;
-                    continue;
-                } else {
-                    p2.right = null;
-                }
-            } else {
-                res.add(p1.val);
-            }
-            p1 = p1.right;
+    public void preOrder(TreeNode cur, List result) {
+        //终止条件
+        if (cur == null) {
+            return;
         }
-        return res;
+        //前序 中左右
+        result.add(cur.val);
+        preOrder(cur.left, result);
+        preOrder(cur.right, result);
     }
 }
 

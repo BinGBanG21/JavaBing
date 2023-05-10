98b0691372ceb1ac942aa476c32ebceb0ba97653
wangbingbing <1345529388@qq.com>
2023-05-10 17:57:02 +0800

    Repeat

--- 每日一题/翻转二叉树/src/InvertTree.java
+++ 每日一题/翻转二叉树/src/InvertTree.java
@@ -5,34 +5,53 @@ public class InvertTree {
 
     }
 
+    //    public TreeNode invertTree(TreeNode root) {
+//        if (root == null) {
+//            return null;
+//        }
+//        TreeNode temp = root.left;
+//        root.left = root.right;
+//        root.right = temp;
+//        invertTree(root.left);
+//        invertTree(root.right);
+//        return root;
+//    }
+    //翻转二叉树
     public TreeNode invertTree(TreeNode root) {
+        //参数处理 终止条件
         if (root == null) {
             return null;
         }
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+        //中-左-右 前序
+        swap(root);
         invertTree(root.left);
         invertTree(root.right);
         return root;
     }
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+    //交换节点
+    public void swap(TreeNode node) {
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
 
-        TreeNode() {
-        }
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode(int val) {
-            this.val = val;
-        }
+    TreeNode() {
+    }
 
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }

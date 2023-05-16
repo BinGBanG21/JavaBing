68b0b57c9571401239c2c670a9d3bc8caf6e2db6
wangbingbing <1345529388@qq.com>
2023-05-16 16:24:53 +0800

    SumOfLeftLeaves

--- 每日一题/左叶子之和/src/SumOfLeftLeaves.java
+++ 每日一题/左叶子之和/src/SumOfLeftLeaves.java
@@ -2,23 +2,39 @@ public class SumOfLeftLeaves {
     public static void main(String[] args) {
 
     }
-    public int sumOfLeftLeaves(TreeNode root) {
-        return root != null ? dfs(root) : 0;
-    }
-
-    public int dfs(TreeNode node) {
-        int ans = 0;
-        if (node.left != null) {
-            ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+//    public int sumOfLeftLeaves(TreeNode root) {
+//        return root != null ? dfs(root) : 0;
+//    }
+//
+//    public int dfs(TreeNode node) {
+//        int ans = 0;
+//        if (node.left != null) {
+//            ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+//        }
+//        if (node.right != null && !isLeafNode(node.right)) {
+//            ans += dfs(node.right);
+//        }
+//        return ans;
+//    }
+//
+//    public boolean isLeafNode(TreeNode node) {
+//        return node.left == null && node.right == null;
+//    }
+    public int sumOfLeftLeaves(TreeNode root){
+        //终止条件
+        if (root == null) {
+            return 0;
         }
-        if (node.right != null && !isLeafNode(node.right)) {
-            ans += dfs(node.right);
+        //后序遍历
+        int leftNum = sumOfLeftLeaves(root.left);
+        int rightNum = sumOfLeftLeaves(root.right);
+        //找到叶子节点的父节点 然后拿到左子节点的值
+        int midVal = 0; //记录左边的值
+        if(root.left != null && root.left.left == null && root.left.right == null){
+            midVal = root.left.val;
         }
-        return ans;
-    }
-
-    public boolean isLeafNode(TreeNode node) {
-        return node.left == null && node.right == null;
+        //中间节点处理
+        return leftNum + rightNum + midVal;
     }
 }
 

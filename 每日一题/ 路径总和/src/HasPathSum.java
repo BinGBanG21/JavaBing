01dee875d3848282dc62ee02f3a113aa8d3993ed
wangbingbing <1345529388@qq.com>
2023-05-20 15:49:04 +0800

    HasPathSum

--- 每日一题/ 路径总和/src/HasPathSum.java
+++ 每日一题/ 路径总和/src/HasPathSum.java
@@ -7,16 +7,51 @@ public class HasPathSum {
 
     }
 
+    //    public boolean hasPathSum(TreeNode root, int sum) {
+//        //空树 直接return
+//        if (root == null) {
+//            return false;
+//        }
+//        if (root.left == null && root.right == null) {
+//            return sum == root.val;
+//        }
+//        //递进条件 root节点两条路走下去 走一次减一个节点
+//        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+//    }
     public boolean hasPathSum(TreeNode root, int sum) {
-        //空树 直接return
-        if (root == null) {
+        if( root == null ){
             return false;
         }
-        if (root.left == null && root.right == null) {
-            return sum == root.val;
+        //减去中间节点的值
+        return traversal(root, sum - root.val);
+    }
+
+    public boolean traversal(TreeNode node, int count) {
+        //叶子节点终止
+        if (node.left == null && node.right == null && count == 0) {
+            return true;
         }
-        //递进条件 root节点两条路走下去 走一次减一个节点
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        if (node.left == null && node.right == null && count != 0){
+            return false;
+        }
+        //单层递归的逻辑 左右节点
+        if (node.left != null) {
+            //减去当前节点的值
+            count -= node.left.val;
+            if (traversal(node.left, count) == true) {
+                return true;
+            }
+            count += node.left.val;
+        }
+        if (node.right != null) {
+            //减去当前节点的值
+            count -= node.right.val;
+            if (traversal(node.right, count) == true) {
+                return true;
+            }
+            count += node.right.val;
+        }
+        return false;
     }
 }
 

e4e8e6f463e457adcd334fed3e8ea5e71604f392
wangbingbing <1345529388@qq.com>
2023-05-20 15:16:05 +0800

    FindBottomLeftValue

--- 每日一题/找树左下角的值/src/FindBottomLeftValue.java
+++ 每日一题/找树左下角的值/src/FindBottomLeftValue.java
@@ -6,21 +6,48 @@ public class FindBottomLeftValue {
 
     }
 
-    public int findBottomLeftValue(TreeNode root) {
-        int ret = 0;
-        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode p = queue.poll();
-            if (p.right != null) {
-                queue.offer(p.right);
+//    public int findBottomLeftValue(TreeNode root) {
+//        int ret = 0;
+//        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+//        queue.offer(root);
+//        while (!queue.isEmpty()) {
+//            TreeNode p = queue.poll();
+//            if (p.right != null) {
+//                queue.offer(p.right);
+//            }
+//            if (p.left != null) {
+//                queue.offer(p.left);
+//            }
+//            ret = p.val;
+//        }
+//        return ret;
+//    }
+    int dep = -1;
+    int result;
+    public int findBottomLeftValue(TreeNode root){
+        return traversal(root);
+    }
+    public int traversal( TreeNode node, int depth){
+        //终止条件
+        if (node.left == null && node.right == null) {
+            if (depth > dep) {
+                //更新深度 记录值
+                dep = depth;
+                result = node.val;
             }
-            if (p.left != null) {
-                queue.offer(p.left);
-            }
-            ret = p.val;
         }
-        return ret;
+        //递进条件
+        if (node.left != null) {
+            depth++;
+            traversal(node.left,depth);
+            depth--;
+        }
+        if (node.right != null) {
+            depth++;
+            traversal(node.right,depth);
+            depth--;
+        }
+        return result;
     }
 }
 

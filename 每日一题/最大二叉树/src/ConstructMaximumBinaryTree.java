510074de3d35746ed5b9af8952a880b1a6ce0110
wangbingbing <1345529388@qq.com>
2023-05-21 15:45:36 +0800

    ConstructMaximumBinaryTree

--- 每日一题/最大二叉树/src/ConstructMaximumBinaryTree.java
+++ 每日一题/最大二叉树/src/ConstructMaximumBinaryTree.java
@@ -7,23 +7,55 @@ public class ConstructMaximumBinaryTree {
 
     }
 
+//    public TreeNode constructMaximumBinaryTree(int[] nums) {
+//        int n = nums.length;
+//        List<Integer> stack = new ArrayList<Integer>();
+//        TreeNode[] tree = new TreeNode[n];
+//        for (int i = 0; i < n; ++i) {
+//            tree[i] = new TreeNode(nums[i]);
+//            while (!stack.isEmpty() && nums[i] > nums[stack.get(stack.size() - 1)]) {
+//                tree[i].left = tree[stack.get(stack.size() - 1)];
+//                stack.remove(stack.size() - 1);
+//            }
+//            if (!stack.isEmpty()) {
+//                tree[stack.get(stack.size() - 1)].right = tree[i];
+//            }
+//            stack.add(i);
+//        }
+//        return tree[stack.get(0)];
+//    }
+
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        int n = nums.length;
-        List<Integer> stack = new ArrayList<Integer>();
-        TreeNode[] tree = new TreeNode[n];
-        for (int i = 0; i < n; ++i) {
-            tree[i] = new TreeNode(nums[i]);
-            while (!stack.isEmpty() && nums[i] > nums[stack.get(stack.size() - 1)]) {
-                tree[i].left = tree[stack.get(stack.size() - 1)];
-                stack.remove(stack.size() - 1);
-            }
-            if (!stack.isEmpty()) {
-                tree[stack.get(stack.size() - 1)].right = tree[i];
-            }
-            stack.add(i);
-        }
-        return tree[stack.get(0)];
+        return construct(nums,0,nums.length);
     }
+
+    public TreeNode construct(int[] nums, int leftIndex, int rightIndex) {
+        //生成树 都采用前序遍历
+        //终止条件
+        if (rightIndex - leftIndex < 1) {
+            return null;
+        }
+        //叶子节点
+        if (rightIndex - leftIndex == 1) {
+            return new TreeNode(nums[leftIndex]);
+        }
+        //中 假设第一个元素为最大值
+        int maxIndex = leftIndex;
+        int maxValue = nums[leftIndex];
+        for (int i = leftIndex; i < rightIndex; i++) {
+            if (nums[i] > maxValue) {
+                maxIndex = i;
+                maxValue = nums[i];
+            }
+
+        }
+        TreeNode root = new TreeNode(maxValue);
+        //左右
+        root.left = construct(nums, leftIndex, maxIndex);
+        root.right = construct(nums, maxIndex + 1, rightIndex);
+        return root;
+    }
+
 }
 
 class TreeNode {

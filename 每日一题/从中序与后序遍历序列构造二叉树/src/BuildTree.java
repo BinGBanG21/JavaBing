66b4e51da5fb2daf3fd3ac119a6936108558d57d
wangbingbing <1345529388@qq.com>
2023-05-21 14:50:35 +0800

    BuildTree

--- 每日一题/从中序与后序遍历序列构造二叉树/src/BuildTree.java
+++ 每日一题/从中序与后序遍历序列构造二叉树/src/BuildTree.java
@@ -7,29 +7,55 @@ public class BuildTree {
 
     }
 
+    //    public TreeNode buildTree(int[] inorder, int[] postorder) {
+//        if (postorder == null || postorder.length == 0) {
+//            return null;
+//        }
+//        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+//        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+//        stack.push(root);
+//        int inorderIndex = inorder.length - 1;
+//        for (int i = postorder.length - 2; i >= 0; i--) {
+//            int postorderVal = postorder[i];
+//            TreeNode node = stack.peek();
+//            if (node.val != inorder[inorderIndex]) {
+//                node.right = new TreeNode(postorderVal);
+//                stack.push(node.right);
+//            } else {
+//                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+//                    node = stack.pop();
+//                    inorderIndex--;
+//                }
+//                node.left = new TreeNode(postorderVal);
+//                stack.push(node.left);
+//            }
+//        }
+//        return root;
+//    }
+    Map<Integer, Integer> map;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (postorder == null || postorder.length == 0) {
+        //把中序数组的元素值和索引记录 方便查找
+        map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return traversal(inorder, 0, inorder.length, postorder, 0, postorder.length);
+    }
+
+    public TreeNode traversal(int[] inorder, int inBegin, int inEnd, int[] postorder, int poBegin, int poEnd) {
+        //终止条件 到叶子节点
+        if (inBegin >= inEnd && poBegin >= poEnd) {
             return null;
         }
-        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
-        Deque<TreeNode> stack = new LinkedList<TreeNode>();
-        stack.push(root);
-        int inorderIndex = inorder.length - 1;
-        for (int i = postorder.length - 2; i >= 0; i--) {
-            int postorderVal = postorder[i];
-            TreeNode node = stack.peek();
-            if (node.val != inorder[inorderIndex]) {
-                node.right = new TreeNode(postorderVal);
-                stack.push(node.right);
-            } else {
-                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
-                    node = stack.pop();
-                    inorderIndex--;
-                }
-                node.left = new TreeNode(postorderVal);
-                stack.push(node.left);
-            }
-        }
+        //找到后序中最后一个元素的位置 然后拿到其索引
+        int rootIndex = map.get(postorder[poEnd - 1]);
+        TreeNode root = new TreeNode(inorder[rootIndex]);
+        //拿到左子树的节点个数
+        int lenOfLeft = rootIndex - inBegin;
+        //生成左右子树 后序采用lenOfLeft进行切割
+        root.left = traversal(inorder, inBegin, rootIndex, postorder, poBegin, poBegin + lenOfLeft);
+        root.right = traversal(inorder, rootIndex + 1, inEnd, postorder, poBegin + lenOfLeft, poEnd - 1);
         return root;
     }
 }

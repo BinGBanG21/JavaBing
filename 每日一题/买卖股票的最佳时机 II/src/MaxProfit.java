c38607ae55d42ae4e9a21fe9fc97f63b8aded741
wangbingbing <1345529388@qq.com>
2023-05-27 20:14:43 +0800

    MaxProfit

--- 每日一题/买卖股票的最佳时机 II/src/MaxProfit.java
+++ 每日一题/买卖股票的最佳时机 II/src/MaxProfit.java
@@ -3,12 +3,20 @@ public class MaxProfit {
 
     }
 
+    //    public int maxProfit(int[] prices) {
+//        int ans = 0;
+//        int n = prices.length;
+//        for (int i = 1; i < n; ++i) {
+//            ans += Math.max(0, prices[i] - prices[i - 1]);
+//        }
+//        return ans;
+//    }
+    //从第二天开始循环 正数就就记录
     public int maxProfit(int[] prices) {
-        int ans = 0;
-        int n = prices.length;
-        for (int i = 1; i < n; ++i) {
-            ans += Math.max(0, prices[i] - prices[i - 1]);
+        int maxPro = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxPro += Math.max(0, prices[i] - prices[i - 1]);
         }
-        return ans;
+        return maxPro;
     }
 }

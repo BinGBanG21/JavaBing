e5c4b0ecb7e1d08914b95c8f467a0cc5b2a11a7d
wangbingbing <1345529388@qq.com>
2023-06-10 19:39:22 +0800

    SortedSquares3

--- 每日一题/有序数组的平方/src/SortedSquares.java
+++ 每日一题/有序数组的平方/src/SortedSquares.java
@@ -19,6 +19,25 @@ public class SortedSquares {
 //        }
 //        return result;
 //    }
+    public int[] sortedSquares3(int[] nums) {
+        //要求返回新数组 所以创建 不可以在原数组循环 会导致一个数越来越大
+        int[] newArr = new int[nums.length];
+        //双指针 一前一后 然后还需要一个指针 用来赋值
+        int left = 0;
+        int right = nums.length - 1;
+        int cur = nums.length - 1;
+        while(left <= right){
+            if(nums[right] * nums[right] >= nums[left] * nums[left]){
+                newArr[cur] = nums[right] * nums[right];
+                right--;
+            }else{
+                newArr[cur] = nums[left] * nums[left];
+                left++;
+            }
+            cur--;
+        }
+        return newArr;
+    }
     public int[] sortedSquares(int[] nums) {
         //首先数组是有序的 且存在负数 所以最大值一定在数组的两端
         //故采用双指针的方式 一头一尾 比较 然后逆序给数组赋值 从而由小到大输出数组

40f4d4e7e8860088bdfc326cf297f2e8542d4693
wangbingbing <1345529388@qq.com>
2023-06-11 15:15:14 +0800

    SQL

---
+++ MySQL/03_基本的SELECT语句/src/SQL.java
@@ -0,0 +1,19 @@
+public class SQL {
+    public static void main(String[] args) {
+
+    }
+    /*
+      SQL分类
+        SQL语言在功能上主要分为如下3大类:
+            DDL (Data Definition Languages、数据定义语言)
+                这些语句定义了不同的数据库、表、视图、索引等数据库对象,还可以用来创建、删除、修改数据库和数据表的结构。
+                主要的语句关键字包括CREATE、DROP 、ALTER等。
+            DML (Data Manipulation Language、数据操作语言)，用于添加、删除、更新和查询数据库记录，并检查数据完整性。
+                主要的语句关键字包括INSERT 、DELETE、UPDATE、SELECT等。SELECT是SQL语言的基础,最为重要。
+            DCL (Data Control Language、数据控制语言)*，用于定义数据库、表、字段、用户的访问权限和安全级别。
+                主要的语句关键字包括GRANT、REVOKE、COMMIT、ROLLBACK、SAVEPOINT等。
+                因为查询语句使用的非常的频繁，所以很多人把查询语句单拎出来一类:DQL(数据查询语言）)。
+                还有单独将COMMIT、ROLLBACK取出来称为TCL (Transaction Control Language，事务控制语言)。
+
+    * */
+}
--- 每日一题/移除元素/src/RemoveElement.java
+++ 每日一题/移除元素/src/RemoveElement.java
@@ -48,7 +48,7 @@ public class RemoveElement {
         int slow = 0;
         for (int fast = 0; fast < nums.length; fast++) {
             if (nums[fast] != val) {
-                nums[slow++] = nums[fast]:
+                nums[slow++] = nums[fast];
             }
         }
         return slow;

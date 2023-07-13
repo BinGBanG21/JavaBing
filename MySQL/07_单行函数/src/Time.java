cddfed2b108699a73185f2f3c39f6b514dbd41dd
wangbingbing <1345529388@qq.com>
2023-07-13 18:57:30 +0800

    Time

---
+++ MySQL/07_单行函数/src/Time.java
@@ -0,0 +1,15 @@
+public class Time {
+    public static void main(String[] args) {
+
+    }
+    /*
+        获取时间、日期
+            函数                        用法
+            CURDATE()                  返回当前的时间 只包含年、月、日
+            CURTIME()                  返回当前的时间 只包含时、分、秒
+            NOW()                      返回当前系统的日期和时间
+            UTC_DATE                   返回世界标准日期
+            UTC_TIME                   返回世界标准时间
+            
+     */
+}

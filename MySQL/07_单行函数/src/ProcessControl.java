54e5a6fbf6b4dcb710baeae997bf0ccb683f12fd
wangbingbing <1345529388@qq.com>
2023-07-16 16:47:54 +0800

    ProcessControl

---
+++ MySQL/07_单行函数/src/ProcessControl.java
@@ -0,0 +1,21 @@
+public class ProcessControl {
+    public static void main(String[] args) {
+
+    }
+    /*
+    流程控制函数
+        流程处理函数可以根据不同的条件，执行不同的处理流程，可以在SQL语句中实现不同的条件选择。
+        MySQL中的流程处理函数主要包括lF()、IFNULL()和CASE()函数。
+        IF(value,value1,value2)                         如果value的值为TRUE，返回value1，否则返回value2
+        IFNULL(value1, value2)                          如果value1不为NULL，返回value1，否则返回value2
+        CASE WHEN条件1 THEN结果1 WHEN条件2                  相当于Java的if...else if...else...
+        THEN结果2... [ELSEresultn] END
+        CASE expr WHEN常量值1THEN值1                      相当于Java的switch...case...
+        WHEN常量值1 THEN值1 ....[ELSE值n]END
+
+
+
+
+
+     */
+}

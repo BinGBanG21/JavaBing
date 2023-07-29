74b22b82d00c198090765f87d71e6c45fe3c7b05
wangbingbing <1345529388@qq.com>
2023-07-29 20:48:43 +0800

    GroupFunc

---
+++ MySQL/08_聚合函数/src/GroupFunc.java
@@ -0,0 +1,18 @@
+public class GroupFunc {
+    public static void main(String[] args) {
+
+    }
+    /*
+    聚合函数
+        聚合(或聚集、分组)函数是对一组数据进行汇总的函数，输入的是一组数据的集合，输出的是单个值。
+        作用与一组数据，并对一组数据返回一个值
+    聚合函数的类型(常用)
+        AVG()
+        SUM()
+        MAX()
+        MIN()
+        COUNT()
+    GROUP BY的使用
+    HAVING关键字的使用
+     */
+}

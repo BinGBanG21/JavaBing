3bd3b83e1e268ea786470527c90d84eee7d2159c
wangbingbing <1345529388@qq.com>
2023-08-01 13:05:58 +0800

    GroupFunc

--- MySQL/08_聚合函数/src/GroupFunc.java
+++ MySQL/08_聚合函数/src/GroupFunc.java
@@ -14,7 +14,27 @@ public class GroupFunc {
           FROM employees;
         MAX()
         MIN()
+          可以用于处理字符串(姓名 日期)
+          SELECT MAX(salary)
+          FROM employees;
         COUNT()
+          计算指定字段在查询结构中出现的个数(统计数量 不包含null值)
+          SELECT COUNT(1) # 1表示把整体第一行所有数据看成1
+          FROM employees;
+          计算表中有多少条记录
+          1.COUNT(*)
+          2.COUNT(1)
+          3.COUNT(具体字段) 不一定对
+            #需求:查询公司中平均奖金率#错误的!
+            SE工ECT AVG (commission pct)
+            FROM employees;
+            #正确的:
+            SELECT SUM(commission_pct) / COUNT (IFNULL ( commission_pct,0))，
+            #AVG(IFNULL(commission_pct,0))
+            FROM employees;
+            如何需要统计表中的记录数，使用CoUNT(*)、COUNT(1)、COUNT(具体字段)哪个效率更高呢?
+            如果使用的是MyISAM存储引擎，则三者效率相同，都是O(1)
+            如果使用的是InnoDB存储引擎，则三者效率:COUNT(*) = COUNT(1)> COUNT(字段)
 
     GROUP BY的使用
     HAVING关键字的使用

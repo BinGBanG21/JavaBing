32341b2c582ff9420218e1304006d9881a4518e1
wangbingbing <1345529388@qq.com>
2023-09-14 22:10:33 +0800

    MultiLine

--- MySQL/09_子查询/src/MultiLine.java
+++ MySQL/09_子查询/src/MultiLine.java
@@ -41,5 +41,7 @@ public class MultiLine {
         GROUP BY department_id
          GROUP BY department_id
         GROUP BY department_id
+        GROUP BY department_id
+        GROUP BY department_id
      */
 }

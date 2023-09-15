d727c9da8636bdc3796e45fec657d70f03c8f9b0
wangbingbing <1345529388@qq.com>
2023-09-15 16:45:44 +0800

    MultiLine

--- MySQL/09_子查询/src/MultiLine.java
+++ MySQL/09_子查询/src/MultiLine.java
@@ -34,14 +34,35 @@ public class MultiLine {
         );
         #查询平均工资最低的部门id
         MySql中聚合函数不能嵌套使用
-        首先查询各个部门的平均工资
-        SELECT AVG(salary)
+        思路:把查询结果看成一张表 注意表一定要有别名
+        首先查询各个部门的平均工资 然后找出最低 最后使用HAVING筛选出聚合函数的结果
+        方式1:
+        SELECT department_id
         FROM employees
         GROUP BY department_id
+        HAVING AVG(salary) = (
+                                SELECT MIN(avg_sal)
+                                FROM(
+                                    SELECT AVG(salary)
+                                    FROM employees
+                                    GROUP BY department_id
+                                ) t_dep_avg_sal
+                            );
+        方式2:ALL关键字
+        SELECT department_id
+        FROM employees
         GROUP BY department_id
-         GROUP BY department_id
-        GROUP BY department_id
-        GROUP BY department_id
-        GROUP BY department_id
+        HAVING AVG(salary) <= ALL (
+                                    SELECT AVG(salary)
+                                    FROM employees
+                                    GROUP BY department_id
+    空值问题(注意内查询中有null值的情况)
+        SELECT last_name
+        FROM employees
+        WHERE employee_id NOT IN (
+                                    SELECT manager_id
+                                    FROM employees
+                                 )
+
      */
 }

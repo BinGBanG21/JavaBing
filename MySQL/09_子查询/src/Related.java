4b51a05e484e246661b29adc42d48eb48dd54d41
wangbingbing <1345529388@qq.com>
2023-09-22 20:16:47 +0800

    Related

--- MySQL/09_子查询/src/Related.java
+++ MySQL/09_子查询/src/Related.java
@@ -55,6 +55,11 @@ public class Related {
 
         HAVING 包含聚合函数的过滤条件
         ORDER BY ..., ...,(ASC/DESC)
-        LIMIT ..., ...
+        LIMIT ..., ...;
+
+    题目: 若employees表中employee id与job history表中employee id相同的数目不小于2,
+          输出这些相同id的员工的employee_id,last_name和其job_id
+        SELECT employee_id, last_name, job_id
+        
      */
 }

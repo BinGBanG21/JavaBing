1d0a843ff8b43268b134294a7edf261c2197c121
wangbingbing <1345529388@qq.com>
2023-11-12 21:49:37 +0800

    CanFinish GetSum Rotate

---
+++ 每日一题/轮转数组/src/Rotate.java
@@ -0,0 +1,24 @@
+public class Rotate {
+    public static void main(String[] args) {
+
+    }
+
+    public void rotate(int[] nums, int k) {
+        //整体反转 然后两个部分再翻转
+         k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k -1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

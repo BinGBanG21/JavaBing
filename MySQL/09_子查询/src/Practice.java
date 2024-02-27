81c83145b890d97a5cfe2f44b30768cf73e22ab3
wangbingbing <1345529388@qq.com>
2024-02-27 20:33:24 +0800

    Practice

--- MySQL/09_子查询/src/Practice.java
+++ MySQL/09_子查询/src/Practice.java
@@ -234,9 +234,105 @@ public class Practice {
                 LIMIT 0,1
             ) t_dept_max_sal
             WHERE e.`department_id` = t_dept_max_sal.`department_id`;
+    14.查询平均工资最高的部门的manager的详细信息：last_name, department_id, email, salary
+        SELECT last_name, department_id, email, salary
+        FROM employees
+        WHERE employee_id IN (
+            SELECT DISTINCT manager_id
+            FROM employees
+            WHERE department_id = (
+                SELECT department_id
+                FROM employees
+                GROUP BY department_id
+                HAVING AVG(salary) = (
+                    SELECT MAX(avg_sal)
+                    FROM (
+                        SELECT AVG(salary) avg_sal
+                        FROM employees
+                        GROUP BY department_id
+                    ) t_dept_avg_sal
+                )
+            )
+        );
 
+        SELECT last_name, department_id, email, salary
+        FROM employees
+        WHERE employee_id IN (
+            SELECT DISTINCT manager_id
+            FROM employees e, (
+                SELECT department_id, AVG(salary) avg_sal
+                FROM employees
+                GROUP BY department_id
+                ORDER BY avg_sal DESC
+                LIMIT 0,1
+            ) t_dept_avg_sal
+            WHERE e.`department_id` = t_dept_avg_sal.`department_id`
+        );
+    15.查询部门的部门号，其中不包括job_id是"ST_CLERK"的部门号
+        SELECT department_id
+        FROM departments
+        WHERE department_id NOT IN (
+            SELECT DISTINCT department_id
+            FROM employees
+            WHERE job_id = `ST_CLERK`
+        );
 
+        SELECT department_id
+        FROM department d
+        WHERE NOT EXISTS (
+            SELECT *
+            FROM employees e
+            WHERE d.`department_id` = e.`department_id`
+            AND e.`job_id` = 'ST_CLERK'
+        );
+    16.选择所有没有管理者的员工的last_name
+        SELECT last_name
+        FROM employees emp
+        WHERE NOT EXISTS (
+            SELECT *
+            FROM employees mgr
+            WHERE emp.`manager_id` = mgr.`employee_id`
+        );
+    17.查询员工号、姓名、雇用时间、工资，其中员工的管理者为 ‘De Haan'
+        SELECT employee_id, last_name, hire_date, salary
+        FROM employee
+        WHERE manager_id IN (
+            SELECT manager_id
+            FROM employee
+            WHERE last_name = 'De Haan'
+        );
+    18.查询各部门中工资比本部门平均工资高的员工的员工号，姓名和工资（相关子查询）
+        SELECT department_id, last_name, salary
+        FROM employees e1
+        WHERE salary > (
+            SELECT AVG(salary)
+            FROM employees e2
+            WHERE e2.`department_id` = e1.`department_id`
+        );
 
-
+        SELECT e.last_name, e.salary, e.department_id
+        FROM employees e, (
+            SELECT department_id, AVG(salary) avg_sal
+            FROM employees
+            GROUP BY department_id
+        ) t_dept_avg_sal
+        WHERE e.`department_id` = t_dept_avg_sal.`department_id`
+        AND e.`salary` > t_dept_avg_sal.`avg_sal`;
+    19.查询每个部门下的部门人数大于5的部门名称（相关子查询）
+        SELECT department_name
+        FROM departments d
+        WHERE 5 < (
+            SELECT COUNT(*)
+            FROM employees e
+            WHERE d.`department_id` = e.`department_id`
+        );
+    20.查询每个国家下的部门个数大于2的国家编号（相关子查询）
+        SELECT country_id
+        FROM locations l
+        WHERE 2 < (
+            SELECT COUNT(*)
+            FROM department d
+            WHERE l.`location_id` = d.`location_id`
+        );
     */
 }

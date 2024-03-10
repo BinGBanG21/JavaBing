02790b64fbc49aa9fc27595bdb26dd6540741c40
wangbingbing <1345529388@qq.com>
2024-03-10 21:35:34 +0800

    IUD

--- MySQL/11_数据处理之增删改/src/IUD.java
+++ MySQL/11_数据处理之增删改/src/IUD.java
@@ -36,6 +36,31 @@ public class IUD {
             * Records：表明插入的记录条数。
             * Duplicates：表明插入时被忽略的记录，原因可能是这 些记录包含了重复的主键值。
             * Warnings：表明有问题的数据值，例如发生数据类型转换。
+        2) 方式2：将查询结果插入到表中
+            INSERT还可以将SELECT语句查询的结果插入到表中，此时不需要把每一条记录的值一个一个输入
+            只需要使用一条INSERT语句和一条SELECT语句组成的组合语句即可快速地从一个或多个表中向一个表中插入多行
+            INSET INTO 目标表名
+            (tar_column1 [, tar_column2, ..., tar_column])
+            SELECT
+            (src_column1 [, src_column2, …, src_column])
+            FROM 源表名
+            [WHERE condition]
+                * 在 INSERT 语句中加入子查询。
+                * 不必书写 VALUES 子句。
+                * 子查询中的值列表应与 INSERT 子句中的列名对应。
+    2.更新数据
+        使用 UPDATE 语句更新数据。语法如下：
+        UPDATE table_name
+        SET column1=value1, column2=value2, ..., column=value
+        [WHERE condition]
+            * 可以一次更新多条数据。
+            * 如果需要回滚数据，需要保证在DML前，进行设置：SET AUTOCOMMIT = FALSE;
+            * 使用 WHERE 子句指定需要更新的数据。
+    3. 删除数据
+        DELETE FROM table_name [WHERE <condition>];
+        table_name指定要执行删除操作的表；“[WHERE ]”为可选参数，指定删除条件，如果没有WHERE子句，
+        DELETE语句将删除表中的所有记录。
+
 
 
 

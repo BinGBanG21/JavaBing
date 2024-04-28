53f87e81a03acb35e9dfcb89163e6f753f6248ec
wangbingbing <1345529388@qq.com>
2024-04-28 15:29:23 +0800

    SaveAndFunc

--- MySQL/15_存储过程与函数/src/SaveAndFunc.java
+++ MySQL/15_存储过程与函数/src/SaveAndFunc.java
@@ -68,6 +68,102 @@ public class SaveAndFunc {
                 4. SELECT… INTO：把从数据表中查询的结果存放到变量中，也就是为变量赋值。
             5、需要设置新的结束标记
                 DELIMITER 新的结束标记
+    3.调用存储过程
+        1) 调用格式
+        存储过程有多种调用方法。存储过程必须使用CALL语句调用，并且存储过程和数据库相关，如果要执行其他数据库中的存储过程，需要指定数据库名称，
+        例如CALL dbname.procname。
+            1、调用in模式的参数：
+                CALL sp1('值');
+            2、调用out模式的参数：
+                SET @name;
+                CALL sp1(@name);
+                SELECT @name;
+            3、调用inout模式的参数：
+                SET @name=值;
+                CALL sp1(@name);
+                SELECT @name;
+    4. 存储函数的使用
+        1) 语法分析
+            语法格式：
+            CREATE FUNCTION 函数名(参数名 参数类型,...)
+            RETURNS 返回值类型
+            [characteristics ...]
+            BEGIN
+            函数体 #函数体中肯定有 RETURN 语句
+            END
+            说明：
+            1、参数列表：指定参数为IN、OUT或INOUT只对PROCEDURE是合法的，FUNCTION中总是默认为IN参数。
+            2、RETURNS type 语句表示函数返回数据的类型； RETURNS子句只能对FUNCTION做指定，对函数而言这是强制的。
+              它用来指定函数的返回类型，而且函 数体必须包含一个 RETURN value 语句。
+            3、characteristic 创建函数时指定的对函数的约束。取值与创建存储过程时相同，这里不再赘述。
+            4、函数体也可以用BEGIN…END来表示SQL代码的开始和结束。如果函数体只有一条语句，也可以省略 BEGIN…END。
+        2) 调用存储函数
+            在MySQL中，存储函数的使用方法与MySQL内部函数的使用方法是一样的。换言之，用户自己定义的存储函数与MySQL内部函数是一个性质的。
+            区别在于，存储函数是 用户自己定义 的，而内部函数是MySQL 的 开发者定义 的。
+            SELECT 函数名(实参列表)
+        3) 对比存储函数与存储过程
+            |          | 关键字    | 调用语法        | 返回值            | 应用场景                         |
+            | -------- | --------- | --------------- | ----------------- | --------------------------  |
+            | 存储过程 | PROCEDURE | CALL 存储过程() | 理解为有0个或多个 | 一般用于更新                     |
+            | 存储函数 | FUNCTION  | SELECT 函数 ()  | 只能是一个        | 一般用于查询结果为一个值并返回时 |
+    5. 存储过程和函数的查看、修改、删除
+        1) 查看
+            MySQL存储了存储过程和函数的状态信息，用户可以使用SHOW STATUS语句或SHOW CREATE语句来查看，
+            也可直接从系统的information_schema数据库中查询。
+            1. 使用SHOW CREATE语句查看存储过程和函数的创建信息
+            SHOW CREATE {PROCEDURE | FUNCTION} 存储过程名或函数名
+            ```
+            2. 使用SHOW STATUS语句查看存储过程和函数的状态信息
+            SHOW {PROCEDURE | FUNCTION} STATUS [LIKE 'pattern']
+            ```
+
+            3. 从information_schema.Routines表中查看存储过程和函数的信息
+            MySQL中存储过程和函数的信息存储在information_schema数据库下的Routines表中。
+            可以通过查询该表的记录来查询存储过程和函数的信息。其基本语法形式如下：
+            SELECT * FROM information_schema.Routines
+            WHERE ROUTINE_NAME='存储过程或函数的名' [AND ROUTINE_TYPE = {'PROCEDURE|FUNCTION'}];
+            ```
+
+            说明：如果在MySQL数据库中存在存储过程和函数名称相同的情况，最好指定ROUTINE_TYPE查询条件来 指明查询的是存储过程还是函数。
+        2) 修改
+            修改存储过程或函数，不影响存储过程或函数功能，只是修改相关特性。使用ALTER语句实现。
+            ALTER {PROCEDURE | FUNCTION} 存储过程或函数的名 [characteristic ...]
+
+            其中，characteristic指定存储过程或函数的特性，其取值信息与创建存储过程、函数时的取值信息略有不同。
+            { CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA }
+            | SQL SECURITY { DEFINER | INVOKER }
+            | COMMENT 'string'
+
+            * CONTAINS SQL ，表示子程序包含SQL语句，但不包含读或写数据的语句。
+            * NO SQL ，表示子程序中不包含SQL语句。
+            * READS SQL DATA ，表示子程序中包含读数据的语句。
+            * MODIFIES SQL DATA ，表示子程序中包含写数据的语句。
+            * SQL SECURITY { DEFINER | INVOKER } ，指明谁有权限来执行。
+              * DEFINER ，表示只有定义者自己才能够执行。
+              * INVOKER ，表示调用者可以执行。
+            * COMMENT 'string' ，表示注释信息。
+        3) 删除
+            删除存储过程和函数，可以使用DROP语句，其语法结构如下：
+            DROP {PROCEDURE | FUNCTION} [IF EXISTS] 存储过程或函数的名
+    6. 关于存储过程使用的争议
+        1) 优点
+            1、存储过程可以一次编译多次使用。存储过程只在创建时进行编译，之后的使用都不需要重新编译， 这就提升了 SQL 的执行效率。
+            2、可以减少开发工作量。将代码 封装 成模块，实际上是编程的核心思想之一，
+               这样可以把复杂的问题 拆解成不同的模块，然后模块之间可以重复使用,在减少开发工作量的同时，还能保证代码的结构清 晰。
+            3、存储过程的安全性强。我们在设定存储过程的时候可以 设置对用户的使用权限 ，这样就和视图一样具 有较强的安全性。
+            4、可以减少网络传输量。因为代码封装到存储过程中，每次使用只需要调用存储过程即可，这样就减 少了网络传输量。
+            5、良好的封装性。在进行相对复杂的数据库操作时，原本需要使用一条一条的 SQL 语句，可能要连接多次数据库才能完成的操作，
+               现在变成了一次存储过程，只需要 连接一次即可 。
+
+        2) 缺点
+            > 阿里开发规范 【强制】禁止使用存储过程，存储过程难以调试和扩展，更没有移植性。
+            1、可移植性差。存储过程不能跨数据库移植，比如在 MySQL、Oracle 和 SQL Server 里编写的存储程，在换成其他数据库时都需要重新编写。
+            2、调试困难。只有少数 DBMS 支持存储过程的调试。对于复杂的存储过程来说，开发和维护都不容易。
+               虽然也有一些第三方工具可以对存储过程进行调试，但要收费。
+            3、存储过程的版本管理很困难。比如数据表索引发生变化了，可能会导致存储过程失效。
+               开发软件的时候往往需要进行版本管理，但是存储过程本身没有版本控制，版本迭代更新的时候很麻烦。
+            4、它不适合高并发的场景。高并发的场景需要减少数据库的压力，有时数据库会采用分库分表的方式，
+               而且对可扩展性要求很高，在这种情况下，存储过程会变得难以维护， 增加数据库的压力 ，显然就不适用了。
 
 
 

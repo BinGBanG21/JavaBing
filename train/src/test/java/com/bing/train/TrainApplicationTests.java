200b70736947a9a19833d757d398e523358a7303
wangbingbing <1345529388@qq.com>
2024-09-06 20:05:32 +0800

    仿12306项目初始化（JDK17+springboot3.0.0+springcloud2022.0.0）

---
+++ train/src/test/java/com/bing/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bing.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

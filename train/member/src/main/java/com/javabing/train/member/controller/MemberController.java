b41c7ead56f6dd46fa52b44c00ba288308b20b8c
wangbingbing <1345529388@qq.com>
2024-09-08 13:04:57 +0800

    集成校验框架Validation

--- train/member/src/main/java/com/javabing/train/member/controller/MemberController.java
+++ train/member/src/main/java/com/javabing/train/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import com.javabing.train.common.resq.CommonResp;
 import com.javabing.train.member.req.MemberRegisterReq;
 import com.javabing.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req) {
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
         long register = memberService.register(req);
         return new CommonResp<>(register);
     }
--- train/member/src/main/java/com/javabing/train/member/req/MemberRegisterReq.java
+++ train/member/src/main/java/com/javabing/train/member/req/MemberRegisterReq.java
@@ -6,10 +6,11 @@ package com.javabing.train.member.req;/*
  * @Create 2024/9/7/星期六 21:51
  * @Version 1.0
  **/
-
+import jakarta.validation.constraints.NotBlank;
 
 public class MemberRegisterReq {
 
+    @NotBlank(message = "【手机号】不能为空")
     private String mobile;
 
     public String getMobile() {

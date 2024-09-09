f3dc1deab03407cb48f38b33e7117884a12b671a
wangbingbing <1345529388@qq.com>
2024-09-09 16:50:06 +0800

    解决跨域问题 玩成登录功能

--- train/web/src/views/login.vue
+++ train/web/src/views/login.vue
@@ -1,63 +1,101 @@
 <template>
-  <a-form
-      :model="formState"
-      name="basic"
-      :label-col="{ span: 8 }"
-      :wrapper-col="{ span: 16 }"
-      autocomplete="off"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-        label="Username"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username" />
-    </a-form-item>
+  <a-row class="login">
+    <a-col :span="8" :offset="8" class="login-main">
+      <h1 style="text-align: center"><rocket-two-tone />&nbsp;JavaBing12306售票系统</h1>
+      <a-form
+          :model="loginForm"
+          name="basic"
+          autocomplete="off"
+      >
+        <a-form-item
+            label=""
+            name="mobile"
+            :rules="[{ required: true, message: '请输入手机号!' }]"
+        >
+          <a-input v-model:value="loginForm.mobile" placeholder="手机号"/>
+        </a-form-item>
 
-    <a-form-item
-        label="Password"
-        name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password" />
-    </a-form-item>
+        <a-form-item
+            label=""
+            name="code"
+            :rules="[{ required: true, message: '请输入验证码!' }]"
+        >
+          <a-input v-model:value="loginForm.code">
+            <template #addonAfter>
+              <a @click="sendCode">获取验证码</a>
+            </template>
+          </a-input>
+          <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
+        </a-form-item>
 
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-    </a-form-item>
+        <a-form-item>
+          <a-button type="primary" block @click="login">登录</a-button>
+        </a-form-item>
 
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-      <a-button type="primary" html-type="submit">Submit</a-button>
-    </a-form-item>
-  </a-form>
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
 import { defineComponent, reactive } from 'vue';
+import axios from 'axios';
+import { notification } from 'ant-design-vue';
+
 export default defineComponent({
+  name: "login-view",
   setup() {
-    const formState = reactive({
-      username: '123',
-      password: '',
-      remember: true,
+    const loginForm = reactive({
+      mobile: '13000000000',
+      code: '',
     });
-    const onFinish = values => {
-      console.log('Success:', values);
+
+    const sendCode = () => {
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        console.log(response);
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
     };
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
+
+    const login = () => {
+      axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          console.log("登录成功：", data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
     };
+
     return {
-      formState,
-      onFinish,
-      onFinishFailed,
+      loginForm,
+      sendCode,
+      login
     };
   },
 });
 </script>
 
 <style>
-
+.login-main h1 {
+  font-size: 25px;
+  font-weight: bold;
+}
+.login-main {
+  margin-top: 100px;
+  padding: 30px 30px 20px;
+  border: 2px solid grey;
+  border-radius: 10px;
+  background-color: #fcfcfc;
+}
 </style>

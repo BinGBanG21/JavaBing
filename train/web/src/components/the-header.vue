6ab5b59526be915225255b4773d0172af8bb3c98
wangbingbing <1345529388@qq.com>
2024-09-11 12:57:02 +0800

    增加乘车人和欢迎页面

--- train/web/src/components/the-header.vue
+++ train/web/src/components/the-header.vue
@@ -8,30 +8,44 @@
       </router-link>
     </div>
     <a-menu
-        v-model:selectedKeys="selectedKeys1"
+        v-model:selectedKeys="selectedKeys"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 11</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
 
 <script>
-import {defineComponent, ref} from 'vue';
+import {defineComponent, ref, watch} from 'vue';
 import store from "@/store";
+import router from '@/router'
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
     let member = store.state.member;
+    const selectedKeys = ref([]);
 
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
     return {
-      selectedKeys1: ref(['2']),
-      member
+      member,
+      selectedKeys
     };
   },
 });
--- train/web/src/views/main.vue
+++ train/web/src/views/main.vue
@@ -3,38 +3,26 @@
     <the-header-view></the-header-view>
     <a-layout>
       <the-sider-view></the-sider-view>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          所有会员总数：{{count}}
-        </a-layout-content>
-      </a-layout>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <router-view></router-view>
+      </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 <script>
-import { defineComponent, ref } from 'vue';
+import {defineComponent} from 'vue';
 import TheHeaderView from "@/components/the-header";
 import TheSiderView from "@/components/the-sider";
-import axios from "axios";
-import {notification} from "ant-design-vue";
-import store from "@/store";
+
 export default defineComponent({
   components: {
     TheSiderView,
     TheHeaderView,
   },
   setup() {
-    const count = ref(0);
-    return {
-      count
-    };
+    return {};
   },
 });
 </script>

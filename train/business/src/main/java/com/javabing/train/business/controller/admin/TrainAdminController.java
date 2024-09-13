67e154707f6deaf34a85b6ee28d3382b3d143ce7
wangbingbing <1345529388@qq.com>
2024-09-13 21:03:21 +0800

    火车车次下拉选择功能开发，增加后端接口

--- train/business/src/main/java/com/javabing/train/business/controller/admin/TrainAdminController.java
+++ train/business/src/main/java/com/javabing/train/business/controller/admin/TrainAdminController.java
@@ -17,6 +17,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/train")
 public class TrainAdminController {
@@ -40,6 +42,12 @@ public class TrainAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 
 }

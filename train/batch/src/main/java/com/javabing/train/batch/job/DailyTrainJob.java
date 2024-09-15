76d66e3ca7b9e96731f41cebffacf0dd1808efb3
wangbingbing <1345529388@qq.com>
2024-09-15 20:56:55 +0800

    为business增加日志拦截器 增加生成每日车次定时任务

---
+++ train/batch/src/main/java/com/javabing/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,31 @@
+package com.javabing.train.batch.job;/*
+ * ClassName: DailyTrainJob
+ * Package: com.javabing.train.batch.job
+ * Description:
+ * @Author WangBing
+ * @Create 2024/9/15/星期日 20:55
+ * @Version 1.0
+ **/
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+    }
+}
---
+++ train/business/src/main/java/com/javabing/train/business/config/SpringMvcConfig.java
@@ -0,0 +1,28 @@
+package com.javabing.train.business.config;/*
+ * ClassName: SpringMvcConfig
+ * Package: com.javabing.train.business.config
+ * Description:
+ * @Author WangBing
+ * @Create 2024/9/15/星期日 20:55
+ * @Version 1.0
+ **/
+
+import com.javabing.train.common.interceptor.LogInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+
+    }
+}

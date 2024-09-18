e91481ab1f523636eaaef13ecbe6a1d16ab77bf9
wangbingbing <1345529388@qq.com>
2024-09-18 13:59:38 +0800

    勾选乘客后，提交时，校验余票是否足够（减轻后端压力）

--- train/web/src/views/main/order.vue
+++ train/web/src/views/main/order.vue
@@ -135,7 +135,7 @@ export default defineComponent({
     const visible = ref(false);
 
     // 勾选或去掉某个乘客时，在购票列表中加上或去掉一张表
-    watch(() => passengerChecks.value, (newVal, oldVal)=>{
+    watch(() => passengerChecks.value, (newVal, oldVal) => {
       console.log("勾选乘客发生变化", newVal, oldVal)
       // 每次有变化时，把购票列表清空，重新构造列表
       tickets.value = [];
@@ -171,6 +171,26 @@ export default defineComponent({
         return;
       }
 
+      // 校验余票是否充足，购票列表中的每个座位类型，都去车次座位余票信息中，看余票是否充足
+      // 前端校验不一定准，但前端校验可以减轻后端很多压力
+      // 注意：这段只是校验，必须copy出seatTypesTemp变量来扣减，用原始的seatTypes去扣减，会影响真实的库存
+      let seatTypesTemp = Tool.copy(seatTypes);
+      for (let i = 0; i < tickets.value.length; i++) {
+        let ticket = tickets.value[i];
+        for (let j = 0; j < seatTypesTemp.length; j++) {
+          let seatType = seatTypesTemp[j];
+          // 同类型座位余票-1，这里扣减的是临时copy出来的库存，不是真正的库存，只是为了校验
+          if (ticket.seatTypeCode === seatType.code) {
+            seatType.count--;
+            if (seatType.count < 0) {
+              notification.error({description: seatType.desc + '余票不足'});
+              return;
+            }
+          }
+        }
+      }
+      console.log("前端余票校验通过");
+
       // 弹出确认界面
       visible.value = true;
 
@@ -200,9 +220,11 @@ export default defineComponent({
   font-size: 18px;
   font-weight: bold;
 }
+
 .order-train .order-train-ticket {
   margin-top: 15px;
 }
+
 .order-train .order-train-ticket .order-train-ticket-main {
   color: red;
   font-size: 18px;
@@ -211,9 +233,11 @@ export default defineComponent({
 .order-tickets {
   margin: 10px 0;
 }
+
 .order-tickets .ant-col {
   padding: 5px 10px;
 }
+
 .order-tickets .order-tickets-header {
   background-color: cornflowerblue;
   border: solid 1px cornflowerblue;
@@ -221,6 +245,7 @@ export default defineComponent({
   font-size: 16px;
   padding: 5px 0;
 }
+
 .order-tickets .order-tickets-row {
   border: solid 1px cornflowerblue;
   border-top: none;

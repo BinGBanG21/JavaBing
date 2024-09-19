21da01fa56f9283544e389b7d6558c20dacaa726
wangbingbing <1345529388@qq.com>
2024-09-19 21:53:25 +0800

    读取配置文件的配置

--- train/member/src/main/java/com/javabing/train/member/controller/TestController.java
+++ train/member/src/main/java/com/javabing/train/member/controller/TestController.java
@@ -1,14 +1,19 @@
 package com.javabing.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return String.format("Hello %s!", testNacos);
     }
 
 }

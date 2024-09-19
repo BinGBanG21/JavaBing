9e5a7836587363689a892c534adc6d4c57bd88fe
wangbingbing <1345529388@qq.com>
2024-09-19 15:18:30 +0800

    修改会员车票页面的显示

--- train/admin/src/views/main/member/ticket.vue
+++ train/admin/src/views/main/member/ticket.vue
@@ -15,7 +15,7 @@
       </template>
       <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
-          <span v-if="item.code === record.col">
+          <span v-if="item.code === record.col && item.type === record.seatType">
             {{item.desc}}
           </span>
         </span>
@@ -69,16 +69,6 @@ export default defineComponent({
     });
     let loading = ref(false);
     const columns = [
-      {
-        title: '会员id',
-        dataIndex: 'memberId',
-        key: 'memberId',
-      },
-      {
-        title: '乘客id',
-        dataIndex: 'passengerId',
-        key: 'passengerId',
-      },
       {
         title: '乘客姓名',
         dataIndex: 'passengerName',
@@ -195,3 +185,4 @@ export default defineComponent({
   },
 });
 </script>
+

dedc7ea7df3e1f45b3e8d5931a15582c2f47c336
wangbingbing <1345529388@qq.com>
2024-09-22 15:23:32 +0800

    配置Gateway+Nacos，实现按应用名来做路由转发

--- train/member/src/main/java/com/javabing/train/member/controller/TestController.java
+++ train/member/src/main/java/com/javabing/train/member/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.javabing.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
 
 @RestController
 @RefreshScope
@@ -13,9 +15,13 @@ public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
     public String hello() {
-        return String.format("Hello %s!", testNacos);
+        String port = environment.getProperty("local.server.port");
+        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 
 }

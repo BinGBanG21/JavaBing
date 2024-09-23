8edaeb9c7b8947078d3b9efb67e30576564170ca
wangbingbing <1345529388@qq.com>
2024-09-23 09:24:21 +0800

    为车次下拉数据增加前端缓存

--- train/admin/src/components/train-select.vue
+++ train/admin/src/components/train-select.vue
@@ -3,8 +3,9 @@
             :filterOption="filterTrainCodeOption"
             @change="onChange" placeholder="请选择车次"
             :style="'width: ' + localWidth">
-    <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :label="item.code + item.start + item.end">
-      {{item.code}} {{item.start}} ~ {{item.end}}
+    <a-select-option v-for="item in trains" :key="item.code" :value="item.code"
+                     :label="item.code + item.start + item.end">
+      {{ item.code }} {{ item.start }} ~ {{ item.end }}
     </a-select-option>
   </a-select>
 </template>
@@ -37,14 +38,22 @@ export default defineComponent({
      * 查询所有的车次，用于车次下拉框
      */
     const queryAllTrain = () => {
-      axios.get("/business/admin/train/query-all").then((response) => {
-        let data = response.data;
-        if (data.success) {
-          trains.value = data.content;
-        } else {
-          notification.error({description: data.message});
-        }
-      });
+      let list = SessionStorage.get(SESSION_ALL_TRAIN);
+      if (Tool.isNotEmpty(list)) {
+        console.log("queryAllTrain 读取缓存");
+        trains.value = list;
+      } else {
+        axios.get("/business/admin/train/query-all").then((response) => {
+          let data = response.data;
+          if (data.success) {
+            trains.value = data.content;
+            console.log("queryAllTrain 保存缓存");
+            SessionStorage.set(SESSION_ALL_TRAIN, trains.value);
+          } else {
+            notification.error({description: data.message});
+          }
+        });
+      }
     };
 
     /**

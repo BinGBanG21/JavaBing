2ba8ec1e2b46710b69c658dff65fd8ee86cf7e6b
wangbingbing <1345529388@qq.com>
2024-09-25 13:18:28 +0800

    出票功能改为循环排队出票，按车次（锁）来循环出票

--- train/business/src/main/java/com/javabing/train/business/mq/ConfirmOrderConsumer.java
+++ train/business/src/main/java/com/javabing/train/business/mq/ConfirmOrderConsumer.java
@@ -8,6 +8,7 @@ package com.javabing.train.business.mq;/*
  **/
 
 import com.alibaba.fastjson.JSON;
+import com.javabing.train.business.dto.ConfirmOrderMQDto;
 import com.javabing.train.business.req.ConfirmOrderDoReq;
 import com.javabing.train.business.service.ConfirmOrderService;
 import jakarta.annotation.Resource;
@@ -15,8 +16,8 @@ import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.slf4j.Logger;
-import org.slf4j.MDC;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -31,9 +32,9 @@ public class ConfirmOrderConsumer implements RocketMQListener<MessageExt> {
     @Override
     public void onMessage(MessageExt messageExt) {
         byte[] body = messageExt.getBody();
-        ConfirmOrderDoReq req = JSON.parseObject(new String(body), ConfirmOrderDoReq.class);
-        MDC.put("LOG_ID", req.getLogId());
-                LOG.info("ROCKETMQ收到消息：{}", new String(body));
-        confirmOrderService.doConfirm(req);
+        ConfirmOrderMQDto dto = JSON.parseObject(new String(body), ConfirmOrderMQDto.class);
+        MDC.put("LOG_ID", dto.getLogId());
+        LOG.info("ROCKETMQ收到消息：{}", new String(body));
+        confirmOrderService.doConfirm(dto);
     }
 }

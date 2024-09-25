e60ea8b5c17e9864a0abf70e1c1632d2a66cf8f3
wangbingbing <1345529388@qq.com>
2024-09-25 21:11:40 +0800

    座位销售图页面获得销售信息

--- train/web/src/views/main/seat.vue
+++ train/web/src/views/main/seat.vue
@@ -9,12 +9,17 @@
     <p>
       日期：{{param.date}}，车次：{{param.trainCode}}，出发站：{{param.start}}，到达站：{{param.end}}
     </p>
+    <p>
+      {{list}}
+    </p>
   </div>
 </template>
 
 <script>
 
-import { defineComponent, ref } from 'vue';
+import {defineComponent, ref, onMounted} from 'vue';
+import axios from "axios";
+import {notification} from "ant-design-vue";
 import {useRoute} from "vue-router";
 
 export default defineComponent({
@@ -23,9 +28,35 @@ export default defineComponent({
     const route = useRoute();
     const param = ref({});
     param.value = route.query;
+    const list = ref();
+
+    // 查询一列火车的所有销售信息
+    const querySeat = () => {
+      axios.get("/business/seat-sell/query", {
+        params: {
+          date: param.value.date,
+          trainCode: param.value.trainCode,
+        }
+      }).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          list.value = data.content;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    onMounted(() => {
+      if (param.value.date) {
+        querySeat();
+      }
+    });
 
     return {
-      param
+      param,
+      querySeat,
+      list
     };
   },
 });

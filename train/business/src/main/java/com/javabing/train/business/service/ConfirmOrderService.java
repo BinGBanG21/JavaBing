cd552aa36f93125ad74411b7951290c717af5ced
wangbingbing <1345529388@qq.com>
2024-09-25 13:29:35 +0800

    MQ消费里，没抢到锁的，表示有其它消费线程正在出票，不做任何处理

--- train/business/src/main/java/com/javabing/train/business/service/ConfirmOrderService.java
+++ train/business/src/main/java/com/javabing/train/business/service/ConfirmOrderService.java
@@ -128,9 +128,8 @@ public class ConfirmOrderService {
         if (Boolean.TRUE.equals(setIfAbsent)) {
             LOG.info("恭喜，抢到锁了！lockKey：{}", lockKey);
         } else {
-            // 只是没抢到锁，并不知道票抢完了没，所以提示稍候再试
-            LOG.info("很遗憾，没抢到锁！lockKey：{}", lockKey);
-            throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_LOCK_FAIL);
+            LOG.info("没抢到锁，有其它消费线程正在出票，不做任何处理");
+            return;
         }
 
         // RLock lock = null;

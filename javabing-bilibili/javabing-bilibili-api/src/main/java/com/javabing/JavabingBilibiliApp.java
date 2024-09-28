657a4895bff2b973c1e255a1a9cb5bca1374d525
wangbingbing <1345529388@qq.com>
2024-09-28 23:24:57 +0800

    项目初始化构建

---
+++ javabing-bilibili/javabing-bilibili-api/src/main/java/com/javabing/JavabingBilibiliApp.java
@@ -0,0 +1,20 @@
+package com.javabing;/*
+ * ClassName: JavabingBilibiliApp
+ * Package: com.javabing
+ * Description:
+ * @Author WangBing
+ * @Create 2024/9/28/星期六 23:20
+ * @Version 1.0
+ **/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class JavabingBilibiliApp {
+    public static void main(String[] args){
+        ApplicationContext app = SpringApplication.run(JavabingBilibiliApp.class, args);
+    }
+}
+

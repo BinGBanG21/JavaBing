7f89b0fe03577217de8ac340376e9c6caf29a0cc
wangbingbing <1345529388@qq.com>
2024-09-28 22:06:58 +0800

    update gitignore

---
+++ binglibingli/binglibingli/src/main/java/com/javabing/Main.java
@@ -0,0 +1,14 @@
+package com.javabing;
+
+/*
+ * ClassName: ${NAME}
+ * Package: com.javabing
+ * Description:
+ * @Author WangBing
+ * @Create 2024/9/28/星期六 18:50
+ * @Version 1.0
+ **/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

4f7f787796e7219806ffa056da1dd52aa733bfd2
wangbingbing <1345529388@qq.com>
2024-09-29 22:45:14 +0800

    引入3种加密工具包

--- javabing-bilibili/javabing-bilibili-api/src/main/java/com/javabing/JavabingBilibiliApp.java
+++ javabing-bilibili/javabing-bilibili-api/src/main/java/com/javabing/JavabingBilibiliApp.java
@@ -9,9 +9,8 @@ package com.javabing;/*
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class JavabingBilibiliApp {
     public static void main(String[] args){
         ApplicationContext app = SpringApplication.run(JavabingBilibiliApp.class, args);

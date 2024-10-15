2ae0fbb6835bdb3b7347308aa416f0953d6bb172
wangbingbing <1345529388@qq.com>
2024-10-15 08:31:25 +0800

    弹幕遮罩添加图片透明化方法

--- javabing-bilibili/javabing-bilibili-service/src/main/java/com/javabing/bilibili/service/util/ImageUtil.java
+++ javabing-bilibili/javabing-bilibili-service/src/main/java/com/javabing/bilibili/service/util/ImageUtil.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 import sun.misc.BASE64Encoder;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -140,5 +141,32 @@ public class ImageUtil {
         JSONObject jsonObject = JSONObject.parseObject(result);
         return jsonObject.getString("access_token");
     }
+
+    public void transferAlpha(File file, File outputFile) throws Exception{
+        InputStream is = new FileInputStream(file);
+        Image image = ImageIO.read(is);
+        ImageIcon imageIcon = new ImageIcon(image);
+        BufferedImage bufferedImage = new BufferedImage(imageIcon.getIconWidth(), imageIcon.getIconHeight(),
+                BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D g2D = (Graphics2D) bufferedImage.getGraphics();
+        g2D.drawImage(imageIcon.getImage(), 0, 0, imageIcon.getImageObserver());
+        int alpha = 0;
+        for (int j1 = bufferedImage.getMinY(); j1 < bufferedImage.getHeight(); j1++) {
+            for (int j2 = bufferedImage.getMinX(); j2 < bufferedImage.getWidth(); j2++) {
+                int rgb = bufferedImage.getRGB(j2, j1);
+                int R = (rgb & 0xff0000) >> 16;
+                int G = (rgb & 0xff00) >> 8;
+                int B = (rgb & 0xff);
+                if (((255 - R) < 30) && ((255 - G) < 30) && ((255 - B) < 30)) {
+                    rgb = ((alpha + 1) << 24) | (rgb & 0x00ffffff);
+                }
+                bufferedImage.setRGB(j2, j1, rgb);
+            }
+        }
+        g2D.drawImage(bufferedImage, 0, 0, imageIcon.getImageObserver());
+        // 输出文件
+        ImageIO.write(bufferedImage, "png", outputFile);
+    }
 }
+
 
